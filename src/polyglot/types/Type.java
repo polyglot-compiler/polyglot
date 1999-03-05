af049607ffa1206fee8f4e27921f6b9a24899cae
Nick Mathewson <nickm@mit.edu>
1999-03-05 22:18:03 +0000

    Added a stub Type class.

---
+++ src/polyglot/types/Type.java
@@ -0,0 +1,17 @@
+/*
+ * Type.java
+ */
+
+package jltools.types;
+
+/**
+ * Type
+ *
+ * Overview:
+ *    A type represents a type in some java-based typesystem.  Each type
+ *    is tied to a particular TypeSystem; types with different TypeSystems
+ *    are incomparable.
+ **/
+public abstract class Type {
+  // Fill this in FIXME
+}
