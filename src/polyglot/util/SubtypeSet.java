a4235b335f38185cfb146a90ca6b7cb5ccef558f
Naveen Sastry <nks1@cornell.edu>
1999-12-04 02:30:53 +0000

    initial version.

--- src/polyglot/util/SubtypeSet.java
+++ src/polyglot/util/SubtypeSet.java
@@ -6,16 +6,33 @@ import java.util.*;
 import jltools.types.*;
 
 /**
- * Set containing a list of types.  adds a type to the set if only if no supertype of the set is in the class.
+ * Class to implement sets containing <code>jltools.types.Type </code>.  
+ * Set membership is based on the subtype relationships.  Thus, if 
+ * <code>S</code> is a supertype of <code>A</code> and <code>B</code>, then
+ * { <code>S</code> } union { <code>A</code>,<code>B</code> } = 
+ * { <code>S</code> }.  Similarily, we remove elements from the set such 
+ * that if <code>s</code> is an element of a set <code>S</code>, then a
+ * call to remove <code>r</code> removes all <code>s</code> iff r is a 
+ * a supertype of s.
  */
 public class SubtypeSet implements java.util.Set
 {
   Vector v; 
+  /**
+   * Creates an empty SubtypeSet
+   */
   public  SubtypeSet()
   {
     v = new Vector();
   }
 
+  /**
+   * Add an element of type <code>jltools.types.Type</code> to the set
+   * only if it has no supertypes already in the set. If we do add it, 
+   * remove any subtypes of <code>o</code>
+   * 
+   * @param o The element to add.
+   */
   public boolean add( Object o)
   {
     boolean bHaveToAdd = true;
@@ -58,6 +75,9 @@ public class SubtypeSet implements java.util.Set
 
   }
 
+  /**
+   * Adds all elements from c into this set.
+   */
   public boolean addAll( Collection c )
   {
     if ( c == null) return false;
@@ -67,11 +87,19 @@ public class SubtypeSet implements java.util.Set
     return bChanged;
   }
 
+  /**
+   * Removes all elements from the set
+   */
   public void  clear()
   {
     v.clear();
   }
 
+  /**
+   * Check whether object <code>o</code> is in the set. Because of the 
+   * semantics of the subtype set, <code>o</code> is in the set iff
+   * it descends from (or is equal to) one of the elements in the set.
+   */
   public boolean contains(Object o)
   {
     if ( o instanceof Type)
@@ -94,6 +122,9 @@ public class SubtypeSet implements java.util.Set
     return false;
   }
 
+  /**
+   * Checks whether all elements of the collection are in the set
+   */
   public boolean containsAll(Collection c)
   {
     for (Iterator i = c.iterator() ; i.hasNext() ; )
@@ -112,11 +143,36 @@ public class SubtypeSet implements java.util.Set
     return v.iterator();
   }
 
+  /**
+   * Removes all elements <code>s</code> in the set such that 
+   * <code>s</code> decends from <code>o</code>
+   *
+   * @return whether or not an element was removed.
+   */
   public boolean remove(Object o )
   {
-    boolean bContains = contains ( o );
-    v.remove (o );
-    return bContains;
+    if ( ! (o instanceof Type ))
+      throw new ClassCastException("Can only add types to the set");
+
+    boolean bRemoved = false;
+    for (Iterator i = v.iterator(); i.hasNext() ; ) 
+    {
+      Type t = (Type)i.next();
+      try
+      {
+        if ( t.equals(o) || t.descendsFrom ( (Type)o ) )
+        {
+          bRemoved = true;
+          i.remove() ; 
+        }      
+      }
+      catch ( SemanticException tce) 
+      {
+        throw new IllegalArgumentException(" Cannont perform typesystem operations: " + 
+                                           tce.getMessage());
+      }
+    }
+    return bRemoved;
   }
   
   public boolean removeAll(Collection c) 
@@ -126,7 +182,7 @@ public class SubtypeSet implements java.util.Set
 
   public boolean retainAll(Collection c) 
   {
-    throw new  UnsupportedOperationException( " Not supported" );
+    throw new UnsupportedOperationException( " Not supported" );
   }
   public int size()
   {
--- src/polyglot/visit/ExceptionChecker.java
+++ src/polyglot/visit/ExceptionChecker.java
@@ -1,8 +1,163 @@
 package jltools.visit;
 
 import jltools.ast.*;
+import jltools.util.*;
+import jltools.types.*;
+import java.util.*;
 
 
+/**
+ * FIXME: SERIOUS FLAW: the error queue is being reused across different 
+ * source files, which isn't correct. 
+ */
 public class ExceptionChecker extends NodeVisitor
-{
+{  
+  SubtypeSet s;
+  ErrorQueue eq;
+  private static List oldExceptionCheckers;
+  static 
+  {
+    oldExceptionCheckers = new ArrayList();
+  }
+
+  public ExceptionChecker( ErrorQueue eq )
+  {
+    this.eq = eq;
+
+    s = new SubtypeSet();
+  }
+
+  public Node override( Node n)
+  {
+    if ( n instanceof TryStatement)
+    {
+      // do something special. this is because the children of a 
+      // try block (namely the catch block) has to be handled a little 
+      // bit differently.
+
+      TryStatement ts = (TryStatement)n;
+      ExceptionChecker ec = new ExceptionChecker( eq ) ;
+      
+      ts.getTryBlock().visit( ec );
+      try
+      {
+        ts.exceptionCheck( ec );
+      }
+      catch ( SemanticException se)
+      {
+        throw new InternalCompilerError("Unexpected Semantic Exception.  It appears that type " +
+                                        "comparisons are not working in the TypeSystem.");
+      }
+      // ec now contains any exceptions that the catch blocks didn't get to.
+      
+      for ( Iterator i = ts.catchBlocks(); i.hasNext() ; )
+      {
+        ((Node)i.next()).visit( ec ) ;
+      }
+
+      if ( ts.getFinallyBlock() != null) 
+        ts.getFinallyBlock().visit ( ec );
+
+      // now, try statement is done. 
+      return n;
+    }
+    return null;
+  }
+  
+  /**
+   * This method is called when we are to perform a "normal" traversal of 
+   * a subtree rooted at <code>n</code>.   At every node, we will push a 
+   * stack frame.  Each child node will add the exceptions that it throws
+   * to this stack frame. For most nodes ( excdeption for the try / catch)
+   * will just aggregate the stack frames.
+   *
+   * @param n The root of the subtree to be traversed.
+   * @return The <code>NodeVisitor</code> which should be used to visit the 
+   *  children of <code>n</code>.
+   *
+   */
+  public NodeVisitor enter( Node n)
+  {
+    return getExceptionChecker(); 
+  }
+
+  /**
+   * Here, we pop the stack frame that we pushed in enter and agregate the 
+   * exceptions.
+   *
+   * @param old The original state of root of the current subtree.
+   * @param n The current state of the root of the current subtree.
+   * @param v The <code>NodeVisitor</code> object used to visit the children.
+   * @return The final result of the traversal of the tree rooted at 
+   *  <code>n</code>.
+   */
+  public Node leave( Node old, Node n, NodeVisitor v)
+  {
+    ExceptionChecker oldEC = (ExceptionChecker)v ;
+    // merge results from the children.
+    s.add ( oldEC.s );
+    
+    // reuse the ExceptionCheckers. saves an allocation.  
+    oldEC.s.clear();
+    synchronized ( oldExceptionCheckers ) 
+    {
+      oldExceptionCheckers.add ( v );
+    }
+
+    // gather exceptions from this node.
+    try
+    {
+      return n.exceptionCheck( this );
+    }
+    catch ( SemanticException se)
+    {
+      throw new InternalCompilerError("Unexpected Semantic Exception.  It appears "
+                                      + " that type comparisons are not working "
+                                      + "in the TypeSystem.");
+    }
+  }
+
+  /**
+   * The ast nodes will use this callback to notify us that they throw an 
+   * exception of type t. This should only be called by MethodExpr node, 
+   * and throw node, since they are the only node which can generate
+   * exceptions.  
+   *
+   * @param t The type of exception that the node throws.
+   */
+  public void throwsException (ClassType t)
+  {
+    s.add ( t ) ;
+  }
+  
+  /**
+   * Method to allow the throws clause and method body to inspect and
+   * modify the throwsSet.
+   */
+  public SubtypeSet getThrowsSet()
+  {
+    return s;
+  }
+
+  private ExceptionChecker getExceptionChecker ()
+  {
+    ExceptionChecker e = null;
+    synchronized ( oldExceptionCheckers )
+    {
+      int size = oldExceptionCheckers.size() ; 
+      if ( size > 0 )
+        e = (ExceptionChecker)oldExceptionCheckers.remove( 
+                    oldExceptionCheckers.size() -1 ) ;
+    }
+    if ( e != null) return e;
+    return new ExceptionChecker(eq); 
+  }
+
+  public void reportError( String description, int iLine)
+  {
+    eq.enqueue( ErrorInfo.SEMANTIC_ERROR, 
+                description, 
+                  iLine);    
+  }
+  
 }
