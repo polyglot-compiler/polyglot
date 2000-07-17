3cadafd28dfd6e4b6d10db22794832f5cb83cd1f
Jed Liu <liujed@cs.cornell.edu>
2000-07-17 15:36:07 +0000

    config script and makefile should now support spaces in path names

--- src/Rules.mk
+++ src/Rules.mk
@@ -15,15 +15,15 @@ JAR_FLAGS		= cf
 JAVADOC_MAIN		= com.sun.tools.javadoc.Main
 JAVADOC_DOCLET		= iContract.doclet.Standard
 JAVADOC_OUTPUT		= $(SOURCE)/javadoc
-JAVADOC_FLAGS		= -mx40m -ms40m -classpath $(JAVADOC_CLASSPATH)
+JAVADOC_FLAGS		= -mx40m -ms40m -classpath "$(JAVADOC_CLASSPATH)"
 
 BIN 			= $(SOURCE)
 CC			= gcc
 
 %.class: %.java
 	@if ! [ -f $@ ] || [ $< -nt $@ ] ; then \
-	    echo $(JC) $(JC_FLAGS) -classpath $(CLASSPATH) $< ; \
-	    $(JC) $(JC_FLAGS) -classpath $(CLASSPATH) $< ; \
+	    echo "$(JC)" "$(JC_FLAGS)" -classpath "$(CLASSPATH)" $< ; \
+	    "$(JC)" "$(JC_FLAGS)" -classpath "$(CLASSPATH)" $< ; \
 	else \
 	    touch $@ ; \
 	fi
@@ -39,14 +39,14 @@ endef
 define javadoc
 -rm -rf $(JAVADOC_OUTPUT)
 -mkdir -p $(JAVADOC_OUTPUT)
-$(JAVA) $(JAVADOC_FLAGS) $(JAVADOC_MAIN) \
+"$(JAVA)" "$(JAVADOC_FLAGS)" $(JAVADOC_MAIN) \
 	-d $(JAVADOC_OUTPUT) \
 	-doclet $(JAVADOC_DOCLET) \
 	-sourcepath $(SOURCE) \
-	-classpath $(CLASSPATH) $(PACKAGES)
+	-classpath "$(CLASSPATH)" $(PACKAGES)
 endef
 
 define yacc
 awk 'BEGIN {FS = "\n"; s = 1} {print $$1, "\t // ", s++}' $< | \
-	$(JAVA) -classpath $(CLASSPATH) java_cup.Main -parser Grm
+	"$(JAVA)" -classpath "$(CLASSPATH)" java_cup.Main -parser Grm
 endef
