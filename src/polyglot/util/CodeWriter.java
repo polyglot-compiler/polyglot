823ad73fa27a2e847c9de7f6f3c807145ad991dc
Andrew Myers <andru@cs.cornell.edu>
2001-03-30 20:56:13 +0000

    New and improved CodeWriter.
    - actually finds the optimal formatting, unlike the old code
    - doesn't bail out when something can't be formatted; keeps doing
      best-effort for the rest of the input

--- src/polyglot/util/CodeWriter.java
+++ src/polyglot/util/CodeWriter.java
@@ -1,8 +1,6 @@
-//
-// Simple CodeWriter -- Andrew C. Myers, March 1999
-//                      mod. by N. Sastry Sept 1999
-//   For use in Cornell University Computer Science 412/413
-//
+/**
+ * CodeWriter -- Andrew C. Myers, March 2001
+ */
 
 package jltools.util;
 
@@ -12,160 +10,318 @@ import java.io.Writer;
 import java.io.IOException;
 import java.util.Vector;
 
+/** 
+ * A <code>CodeWriter</code> is a pretty-printing engine.
+ * It formats structured text onto an output stream <code>o</code> in the
+ * minimum number of lines, while keeping the width of the output
+ * within <code>width</code> characters if possible.
+ */
 public class CodeWriter
 {
-    // A code-writer formats text onto an
-    // output stream "o" while keeping the width of the output
-    // within "width" characters if possible
+    /**
+     * Create a CodeWriter object with output stream <code>o</code>
+     * and width <code>width_</code>.
+     */
+
     public CodeWriter(OutputStream o, int width_) {
         output = new OutputStreamWriter(o);
         width = width_;
         current = input = new Block(null, 0);
     }
 
+    /**
+     * Create a CodeWriter object with output <code>w</code> and
+     * width <code>width_</code>.
+     */
     public CodeWriter(Writer w, int width_) {
         output = w;
         width = width_;
         current = input = new Block(null, 0);
     }
         
+    /** Print the string <code>s</code> verbatim on the output stream. */
     public void write(String s) {
-        // Print the string "s" on the output stream
-        if(s != null)
+       if (s.length() > 0)
           current.add(new StringItem(s));
     }
 
+    /** Force a newline with no added indentation. */
     public void newline()
     {
        newline(0);
     }
 
+    /**
+     * Start a new block with a relative indentation of <code>n</code>
+     * characters.
+     * <br>
+     * A block is a formatting unit. The formatting algorithm will try
+     * to put the whole block in one line unless
+     * <ul>
+     * <li>there is a <code>newline</code> item in the block.</li>
+     * <li>the block cannot fit in one line.</li>
+     * </ul>
+     * If either of the two conditions is satisfied, the
+     * formatting algorithm will break the block into lines: every
+     * <code>allowBreak</code> will cause a line change, the first line
+     * is printed at the current cursor position <code>pos</code>,
+     * all the following lines are printed at the position
+     * <code>pos+n</code>.
+     * 
+     * @param n the number of characters increased on indentation (relative
+     * to the current position) for all lines in the block.
+     */         
     public void begin(int n) {
-        // Start a new block with indentation increased
-        // by "n" characters
         Block b = new Block(current, n);
         current.add(b);
         current = b;
     }
+        
+    /** 
+     * Terminate the most recent outstanding <code>begin</code>. 
+     */
     public void end() {
-        // Terminate the most recent outstanding "begin"
         current = current.parent;
         if (current == null) throw new RuntimeException();
     }
+
+    /**
+     * Allow a newline. Indentation will be preserved.
+     * If no newline is inserted, a single space character is output instead.
+     *
+     * @param n the amount of increase in indentation if
+     * the newline is inserted.
+     */ 
     public void allowBreak(int n) {
-        // Allow a newline. Indentation will be preserved.
-	// If no newline is inserted, the string " " is output instead.
         current.add(new AllowBreak(n, " "));
     }
+
+    /**
+     * Allow a newline. Indentation will be preserved.
+     *
+     * @param n the amount of increase in indentation if
+     *  the newline is inserted.
+     * @param alt if no newline is inserted, the string <code>alt</code> is
+     *  output instead.   
+     */ 
     public void allowBreak(int n, String alt) {
-        // Allow a newline. Indentation will be preserved.
-	// If no newline is inserted, the string "alt" is output instead.
         current.add(new AllowBreak(n, alt));
     }
+
+    /**
+     * Force a newline. Indentation will be preserved.  This method
+     * should be used sparingly; usually a call to <code>allowBreak</code> is
+     * preferable because forcing a newline also causes all breaks
+     * in containing blocks to be broken.
+     */
     public void newline(int n) {
-        // Force a newline. Indentation will be preserved.
-	// This method should be used sparingly; usually a call to
-	// "allowBreak" is preferable.
         current.add(new Newline(n));
     }
-    public void flush() throws IOException {
-        // Send out the current batch of text
-        // to be formatted, closing all
-        // outstanding "begin"'s and resetting
-        // the indentation level to 0.
+
+    /**
+     * Send out the current batch of text to be formatted. All
+     * outstanding <code>begin</code>'s are closed and the current
+     * indentation level is reset to 0. Returns true if formatting
+     * was completely successful (the margins were obeyed).
+     */
+    public boolean flush() throws IOException {
+	boolean success = true;
         try {
-            input.format(0, 0, width, true);
+            input.format1(0, 0, width, width, true, true);
         } catch (Overrun e) {
+	    success = false;
         }
         input.sendOutput(output, 0, 0);
         output.flush();
         input.free();
         current = input = new Block(null, 0);
+	return success;
     }
-    public void quickflush() throws IOException {
-        // Send out the current batch of text
-        // to be formatted, closing all
-        // outstanding "begin"'s and resetting
-        // the indentation level to 0.
-        input.sendOutput(output, 0, 0);
-        output.flush();
-        input.free();
-        current = input = new Block(null, 0);
-    }
-    
+
     Block input;
     Block current;
 
     Writer output;
     int width;
+    public static final boolean debug = false;
 }
 
-class Overrun extends Exception {
-    // An overrun represents a formatting that failed because the right
-    // margin was exceeded by at least "amount" chars.
+/**
+ * An <code>Overrun</code> represents a formatting that failed because the right
+ * margin was exceeded by at least <code>amount</code> chars.
+ */
+class Overrun extends Exception 
+{
     int amount;
-    Overrun(int amount_) { amount = amount_; }  
+    Overrun(int amount_) {
+	amount = amount_;
+	if (CodeWriter.debug) System.err.println("Overrun: " + amount);
+    }  
 }
 
-abstract class Item {
-    Item() { next = null; }
-    int format(int lmargin, int pos, int rmargin, boolean canBreak)
-        throws Overrun {
-        // try to format a whole sequence of items in the manner of format1
-        if (pos > rmargin) throw new Overrun(pos - rmargin);
-        pos = format1(lmargin, pos, rmargin, canBreak);
-        if (next == null) return pos;
-        else return next.format(lmargin, pos, rmargin, canBreak);
-    }
-    abstract int format1(int lmargin, int pos, int rmargin, boolean canBreak)
-        throws Overrun;
-        // Try to format this item with a current cursor position of
-        // "pos", left and right margins as specified. Returns the
-        // final position. If breaks
-        // may be broken, "canBreak" is set. Return the new cursor
-        // position and set any contained breaks appropriately if formatting
-        // was successful. Requires rmargin > lmargin, pos <= rmargin.
+/**
+ * An <code>Item</code> is a piece of input handed to the formatter. It
+ * contains a reference to a possibly empty list of items that follow it.
+ */
+abstract class Item 
+{
+    Item next;
+
+    protected Item() { next = null; }
+
+    /** 
+     * Try to format this item. The current cursor position is
+     * <code>pos</code>, left and right margins are as
+     * specified. Returns the final position, which must be
+     * <code>&lt; fin</code>. If breaks may be broken,
+     * <code>can_break</code> is set. Return the new cursor position
+     * and set any contained breaks appropriately if formatting was
+     * successful. If <code>nofail</code>, formatting is forced to 
+     * "succeed" (best effort) even if it doesn't; <code>nofail</code>
+     * requires <code>can_break</code>.
+     *
+     * Requires: rmargin &lt; lmargin, pos &lt;= rmargin.
+     */
+    abstract int format1(int lmargin, int pos, int rmargin, int fin,
+			     boolean can_break, boolean nofail) throws Overrun;
+
+    /**
+     * Send the output associated with this item to <code>o</code>, using the
+     * current break settings.
+     */
     abstract int sendOutput(Writer o, int lmargin, int pos)
       throws IOException;
-        // Send the output associated with this item to "o", using the
-        // current break settings.
+
+    /** Free references to any other items. */
     void free() { 
-        // Free references to any other items.
         if( next != null) { 
             next.free(); 
             next = null; 
         }
     }
-    Item next;
+
+    /** try to format a whole sequence of items in the manner of format1 */
+    static int formatList(Item it, int lmargin, int pos, int rmargin, int fin,
+	  	          boolean can_break, boolean nofail) throws Overrun {
+	if (CodeWriter.debug)
+	    System.err.println("Formatting list " + it +
+		"\n  lmargin = "  + lmargin + " pos = " + pos +
+		" fin = " + fin +
+		(can_break ? " can break" : " no breaks") +
+                (nofail ? " [no fail]" : ""));
+	int this_fin = rmargin;
+	boolean thisnofail = false;
+	if (it.next == null) this_fin = fin;
+	    // this_fin is final position bound for current item. We crank
+	    // it in from the right margin as subsequent items overrun.
+	while (true) {
+	    int nextpos;
+	    try {
+		nextpos = it.format1(lmargin, pos, rmargin, this_fin,
+			             can_break, thisnofail);
+	    } catch (Overrun o) {
+		if (!nofail) throw o;
+		if (CodeWriter.debug && thisnofail) {
+		    System.err.println("Failed with nofail!");
+		    throw o;
+		}
+		thisnofail = true;
+		continue;
+	    }
+
+	    if (it.next == null) return nextpos;
+	    try {
+		return formatList(it.next, lmargin, nextpos, rmargin, fin,
+				  can_break, nofail);
+	    } catch (Overrun o) {
+		if (!can_break) throw o;
+		if (nextpos > this_fin) {
+		    nextpos = this_fin;
+		    if (!nofail) {
+			System.err.println("Formatting failed silently!");
+		    }
+		    thisnofail = true;
+		}
+		this_fin = nextpos - o.amount;
+		if (this_fin < lmargin) throw o;
+		if (CodeWriter.debug)
+		    System.err.println("  Trying list " +
+			it.next + " again with fin = " + this_fin);
+		// try again!
+	    }
+	}
+    }
+
+    public String toString() {
+	if (next == null) return selfToString();
+	return selfToString() + "," + next.toString();
+    }
+
+    abstract String selfToString();
 }
 
+/**
+ * A Block is a formatting unit containing a list of other items
+ * to be formatted.
+ */
 class Block extends Item {
     Block parent;
     Item first;
     Item last;
     int indent;
+        
     Block(Block parent_, int indent_) {
         parent = parent_;
         first = last = null;
         indent = indent_;
     }
+        
+    /**
+     * Add a new item to the end of the block. Successive
+     * StringItems are concatenated together to limit recursion
+     * depth when formatting.
+     */
     void add(Item it) {
-        if (first == null) first = it; else last.next = it;
+        if (first == null) {
+	    first = it;
+	} else {
+	    if (it instanceof StringItem && last instanceof StringItem) {
+		StringItem lasts = (StringItem)last;
+		lasts.appendString(((StringItem)it).s);
+		return;
+	    } else {
+		last.next = it;
+	    }
+	}
         last = it;
     }
-    int format1(int lmargin, int pos, int rmargin, boolean canBreak)
+
+    int format1(int lmargin, int pos, int rmargin, int fin, boolean can_break,
+		boolean nofail)
         throws Overrun
     {
+	if (CodeWriter.debug)
+            System.err.println("Block format " + this + "\n  lmargin = " +
+                "lmargin + pos = " + pos + " fin = " + fin +
+		(can_break ? " can break" : " no breaks") +
+                (nofail ? " [no fail]" : ""));
         if (first == null) return pos;
         try {
-            return first.format(pos + indent, pos, rmargin, false);
+	    return formatList(first, pos + indent, pos, rmargin, fin, false, false);
         } catch (Overrun overrun) {
-            if (!canBreak) throw overrun;
-            return first.format(pos + indent, pos, rmargin, true);
+            if (!can_break) throw overrun;
+	    try {
+		return formatList(first, pos + indent, pos, rmargin, fin, true,
+				  false);
+	    } catch (Overrun o) {
+		if (!nofail) throw o;
+		return formatList(first, pos + indent, pos, rmargin, fin, true,
+				  true);
+	    }
         }
     }
-    int sendOutput(Writer o, int lmargin, int pos)
-            throws IOException {
+
+    int sendOutput(Writer o, int lmargin, int pos) throws IOException {
         Item it = first;
         lmargin = pos+indent;
         while (it != null) {
@@ -174,6 +330,7 @@ class Block extends Item {
         }
         return pos;
     }
+
     void free() {
         super.free();
   
@@ -183,38 +340,54 @@ class Block extends Item {
         }
         last = null;
     }
+    String selfToString() {
+	return "[" + first.toString() + "]";
+    }
 }
 
 class StringItem extends Item {
     String s;
     StringItem(String s_) { s = s_; }
-    int format1(int lmargin, int pos, int rmargin, boolean canBreak) 
+        
+    int format1(int lmargin, int pos, int rmargin, int fin, boolean can_break,
+		boolean nofail) 
         throws Overrun {
         pos += s.length();
-        if (pos > rmargin) throw new Overrun(rmargin - pos);
+        if (pos > fin && !nofail) throw new Overrun(pos - fin);
         return pos;
     }
     int sendOutput(Writer o, int lm, int pos) throws IOException {
         o.write(s);
         return pos + s.length();
     }
+    void appendString(String s) { this.s = this.s + s; }
+    String selfToString() { return s; }
 }
 
-class AllowBreak extends Item {
+class AllowBreak extends Item 
+{
     int indent;
     boolean broken = true;
     String alt;
+        
     AllowBreak(int n_, String alt_) { indent = n_; alt = alt_; }
-    int format1(int lmargin, int pos, int rmargin, boolean canBreak) 
+        
+    int format1(int lmargin, int pos, int rmargin, int fin, boolean can_break,
+		boolean nofail) 
         throws Overrun {
-        if (canBreak) {
+	int ret;
+        if (can_break || nofail) {
             broken = true;
-            return lmargin + indent;
+	    ret = lmargin + indent;
         } else {
             broken = false;
-            return pos + alt.length();
+	    ret = pos + alt.length();
         }
+	if (ret > fin && !nofail)
+	    throw new Overrun(ret - fin);
+	return ret;
     }
+        
     int sendOutput(Writer o, int lmargin, int pos)
         throws IOException {
         if (broken) {
@@ -226,16 +399,22 @@ class AllowBreak extends Item {
             return pos + alt.length();
         }
     }
+
+    String selfToString() { return "^"; }
 }
 
-class Newline extends AllowBreak {
+class Newline extends AllowBreak 
+{
     Newline(int n_) { super(n_, ""); }
-    int format1(int lmargin, int pos, int rmargin, boolean canBreak, boolean forceBreak) 
+        
+    int format1(int lmargin, int pos, int rmargin, int fin, boolean can_break,
+		boolean nofail) 
         throws Overrun {
-        if (!canBreak) throw new Overrun(1);
+        if (!can_break) throw new Overrun(1);
         broken = true;
         return lmargin + indent;
     }
+        
     int sendOutput(Writer o, int lmargin, int pos)
             throws IOException {
         o.write("\r\n");
