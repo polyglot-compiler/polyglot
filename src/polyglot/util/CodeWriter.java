b6a2f6a727dc730e6ba51f5e249a34a4723e465e
Andrew Myers <andru@cs.cornell.edu>
2001-04-06 18:31:20 +0000

    Fixed performance bug in CodeWriter

--- src/polyglot/util/CodeWriter.java
+++ src/polyglot/util/CodeWriter.java
@@ -126,16 +126,21 @@ public class CodeWriter
      */
     public boolean flush() throws IOException {
 	boolean success = true;
-        try {
-            input.format1(0, 0, width, width, true, true);
-        } catch (Overrun e) {
-	    success = false;
-        }
+	try {
+	    Item.format(input,0, 0, width, width, true, true);
+	} catch (Overrun o) { success = false; }
         input.sendOutput(output, 0, 0);
         output.flush();
         input.free();
         current = input = new Block(null, 0);
 	return success;
+    }
+    /**
+     * Return a readable representation of all the structured input
+     * given to the CodeWriter since the last flush.
+     */
+    public String toString() {
+	return input.toString();
     }
 
     Block input;
@@ -144,6 +149,7 @@ public class CodeWriter
     Writer output;
     int width;
     public static final boolean debug = false;
+    public static boolean precompute = false;
 }
 
 /**
@@ -155,7 +161,7 @@ class Overrun extends Exception
     int amount;
     Overrun(int amount_) {
 	amount = amount_;
-	if (CodeWriter.debug) System.err.println("Overrun: " + amount);
+	if (CodeWriter.debug) System.err.println("-- Overrun: " + amount);
     }  
 }
 
@@ -170,21 +176,24 @@ abstract class Item
     protected Item() { next = null; }
 
     /** 
-     * Try to format this item. The current cursor position is
-     * <code>pos</code>, left and right margins are as
-     * specified. Returns the final position, which must be
-     * <code>&lt; fin</code>. If breaks may be broken,
-     * <code>can_break</code> is set. Return the new cursor position
-     * and set any contained breaks appropriately if formatting was
-     * successful. If <code>nofail</code>, formatting is forced to 
-     * "succeed" (best effort) even if it doesn't; <code>nofail</code>
-     * requires <code>can_break</code>.
+     * Try to format this item and subsequent items. The current cursor
+     * position is <code>pos</code>, left and right margins are as
+     * specified. Returns the final position, which must be <code>&lt;
+     * fin</code>. If breaks may be broken, <code>can_break</code> is
+     * set. Return the new cursor position (which may overrun rmargin,
+     * fin, or both, and set any contained breaks accordingly.  (It is
+     * important that formatN not necessarily convert overruns in its
+     * final position into exceptions. This allows the calling routine
+     * to distinguish between 'internal' overruns and ones that it can
+     * tack on a conservative estimate of how much formatting the rest
+     * of the list will make the overrun go up by.  Also, it simplifies
+     * the coding of formatN.)
      *
      * Requires: rmargin &lt; lmargin, pos &lt;= rmargin.
      */
-    abstract int format1(int lmargin, int pos, int rmargin, int fin,
-			     boolean can_break, boolean nofail) throws Overrun;
 
+    abstract int formatN(int lmargin, int pos, int rmargin, int fin,
+			     boolean can_break, boolean nofail) throws Overrun;
     /**
      * Send the output associated with this item to <code>o</code>, using the
      * current break settings.
@@ -200,64 +209,143 @@ abstract class Item
         }
     }
 
-    /** try to format a whole sequence of items in the manner of format1 */
-    static int formatList(Item it, int lmargin, int pos, int rmargin, int fin,
+    /**
+     * Try to format a whole sequence of items in the manner of formatN.
+     * The initial position may be an overrun (this is the only way
+     * that overruns are checked!) <code>it</code> may be also null,
+     * signifying an empty list.
+     */
+    static int format(Item it, int lmargin, int pos, int rmargin, int fin,
 	  	          boolean can_break, boolean nofail) throws Overrun {
-	if (CodeWriter.debug)
-	    System.err.println("Formatting list " + it +
-		"\n  lmargin = "  + lmargin + " pos = " + pos +
-		" fin = " + fin +
+	if (CodeWriter.debug) {
+	    System.err.println("Format: " + it + "\n  lmargin = " +
+		lmargin + " pos = " + pos + " fin = " + fin +
 		(can_break ? " can break" : " no breaks") +
-                (nofail ? " [no fail]" : ""));
-	int this_fin = rmargin;
-	boolean thisnofail = false;
-	if (it.next == null) this_fin = fin;
-	    // this_fin is final position bound for current item. We crank
-	    // it in from the right margin as subsequent items overrun.
-	while (true) {
-	    int nextpos;
-	    try {
-		nextpos = it.format1(lmargin, pos, rmargin, this_fin,
-			             can_break, thisnofail);
-	    } catch (Overrun o) {
-		if (!nofail) throw o;
-		if (CodeWriter.debug && thisnofail) {
-		    System.err.println("Failed with nofail!");
-		    throw o;
+		(nofail ? " [nofail]" : ""));
+	}
+	if (!nofail && pos > rmargin) { // overrun
+	    if (CodeWriter.precompute) {
+		if (CodeWriter.debug) {
+		    System.err.println("MinWidth of " + it + " = " +
+					getMinWidth(it));
+		    System.err.println("MinPosWidth of " + it + " = " +
+					getMinPosWidth(it));
+		    System.err.println("MinIndent of " + it + " = " +
+					getMinIndent(it));
 		}
-		thisnofail = true;
-		continue;
-	    }
-
-	    if (it.next == null) return nextpos;
-	    try {
-		return formatList(it.next, lmargin, nextpos, rmargin, fin,
-				  can_break, nofail);
-	    } catch (Overrun o) {
-		if (!can_break) throw o;
-		if (nextpos > this_fin) {
-		    nextpos = this_fin;
-		    if (!nofail) {
-			System.err.println("Formatting failed silently!");
-		    }
-		    thisnofail = true;
-		}
-		this_fin = nextpos - o.amount;
-		if (this_fin < lmargin) throw o;
-		if (CodeWriter.debug)
-		    System.err.println("  Trying list " +
-			it.next + " again with fin = " + this_fin);
-		// try again!
+		int amount = pos + getMinPosWidth(it) - rmargin;
+		int amount1 = lmargin + getMinWidth(it) - rmargin;
+		if (amount1 > amount) amount = amount1;
+		int amount2 = lmargin + getMinIndent(it) - fin;
+		if (amount2 > amount) amount = amount2;
+		throw new Overrun(amount);
+	    } else {
+		throw new Overrun(pos - rmargin);
 	    }
 	}
+	if (it == null) { // no items to format. Check against final position.
+	    if (!nofail && pos > fin) throw new Overrun(pos - fin);
+	    return pos;
+	}
+	return it.formatN(lmargin, pos, rmargin, fin, can_break, nofail);
+    }
+  
+/*
+    The following fields keep track of the tightest formatting that is
+    possible with an item and its following items, if all breaks are
+    broken.  Formatting is measured relative to both "lmargin" and to
+    "pos".
+
+   lmargin
+   | pos
+   | |
+   | xxxxx
+   xxxxxxxx
+   xxxxxx
+   <------>
+   min_width (at least min_pos_width)
+     <--->
+     min_pos_width
+   <---->
+   min_indent (at most min_width)
+*/
+
+    /** Minimum lmargin-rhs width. */
+    int min_width = -1;
+    
+    /** Minimum lmargin-final offset */
+    int min_indent = -1;
+
+    /** Minimum pos-rhs width (i.e., min width up to first break) */
+    int min_pos_width = -1;
+
+    /** Are there any breaks in this items and following items? */
+    boolean contains_brks;
+    boolean cb_init = false;
+
+    static int max(int i, int j) {
+	if (i > j) return i;
+	return j;
+    }
+
+    static int getMinWidth(Item it) {
+	if (it == null) return 0;
+	if (it.min_width >= 0) return it.min_width;
+	int p1 = it.selfMinWidth();
+	int p2 = it.selfMinIndent();
+	int p3 = getMinPosWidth(it.next) + p2;
+	int p4 = getMinWidth(it.next);
+	return (it.min_width = max(max(p1, p3), p4));
+    }
+
+    static int getMinPosWidth(Item it) {
+	if (it == null) return 0;
+	if (it.min_pos_width >= 0) return it.min_pos_width;
+	int p1 = it.selfMinPosWidth();
+	if (it.next == null ||
+	    it.selfContainsBreaks()) return p1;
+	return p1 + getMinPosWidth(it.next);
+    }
+
+    static int getMinIndent(Item it) {
+	if (it == null) return 0;
+	if (it.min_indent >= 0) return it.min_indent;
+	int p1 = it.selfMinIndent();
+	if (it.next == null) return p1;
+	if (containsBreaks(it.next))
+	    return getMinIndent(it.next);
+	return p1 + getMinPosWidth(it.next);
+    }
+
+    static boolean containsBreaks(Item it) {
+	if (it == null) return false;
+	if (it.cb_init) return it.contains_brks;
+	if (it.selfContainsBreaks()) {
+	    it.contains_brks = true;
+	    it.cb_init = true;
+	    return true;
+	}
+	if (it.next == null) return false;
+	it.contains_brks = containsBreaks(it.next);
+	it.cb_init = true;
+	return it.contains_brks;
+    }
+
+    public String summarize(String s) {
+	if (s.length() <= 60) return s;
+	return s.substring(0, 57) + "...";
     }
 
     public String toString() {
-	if (next == null) return selfToString();
-	return selfToString() + "," + next.toString();
+	if (next == null) return summarize(selfToString());
+	return summarize(selfToString() + next.toString());
     }
 
     abstract String selfToString();
+    abstract int selfMinIndent();
+    abstract int selfMinWidth();
+    abstract int selfMinPosWidth();
+    abstract boolean selfContainsBreaks();
 }
 
 /**
@@ -296,29 +384,50 @@ class Block extends Item {
         last = it;
     }
 
-    int format1(int lmargin, int pos, int rmargin, int fin, boolean can_break,
-		boolean nofail)
-        throws Overrun
-    {
+int formatN(int lmargin, int pos, int rmargin, int fin, boolean can_break,
+    		boolean nofail) throws Overrun {
 	if (CodeWriter.debug)
             System.err.println("Block format " + this + "\n  lmargin = " +
-                "lmargin + pos = " + pos + " fin = " + fin +
+                lmargin + " pos = " + pos + " fin = " + fin +
 		(can_break ? " can break" : " no breaks") +
-                (nofail ? " [no fail]" : ""));
-        if (first == null) return pos;
-        try {
-	    return formatList(first, pos + indent, pos, rmargin, fin, false, false);
-        } catch (Overrun overrun) {
-            if (!can_break) throw overrun;
+		(nofail ? " [nofail]" : ""));
+	    // "this_fin" is a final-position bound for the formatting of
+	    // the contained list, cranked in from the right margin
+	    // when subsequent items overrun.
+	int this_fin = rmargin - getMinPosWidth(next);
+	    // Keep track of whether to send "nofail" down to contained
+	    // list. Don't do this unless forced to.
+	boolean this_nofail = false;
+	    // Keep track of whether to send "can_break" down to contained
+	    // list. Don't do this unless forced to.
+	boolean this_break = false;
+	while (true) {
+	    int next_pos;
 	    try {
-		return formatList(first, pos + indent, pos, rmargin, fin, true,
-				  false);
+	      next_pos = format(first, pos + indent, pos, rmargin,
+				  this_fin, this_break, this_nofail && this_break);
 	    } catch (Overrun o) {
-		if (!nofail) throw o;
-		return formatList(first, pos + indent, pos, rmargin, fin, true,
-				  true);
+		if (!can_break) throw o;
+		if (!this_break) { this_break = true; continue; }
+		if (this_nofail) throw new Error("Failed with this_nofail");
+		if (nofail) { this_nofail = true; continue; }
+		throw o;
 	    }
-        }
+	    try {
+		return format(next, lmargin, next_pos, rmargin, fin,
+			      can_break, nofail);
+	    } catch (Overrun o) {
+		if (!can_break) throw o; // no way to fix it
+		if (next instanceof AllowBreak) throw o; // not our fault
+		this_break = true;
+		if (nofail) throw new Error("Failed with nofail");
+		if (next_pos > this_fin) next_pos = this_fin;
+		this_fin = next_pos - o.amount;
+		if (CodeWriter.debug)
+		    System.err.println("  Trying block again with fin = " +
+				       this_fin);
+	    }
+	}
     }
 
     int sendOutput(Writer o, int lmargin, int pos) throws IOException {
@@ -340,8 +449,32 @@ class Block extends Item {
         }
         last = null;
     }
+
+    int selfMinWidth() {
+	return getMinWidth(first);
+    }
+
+    int selfMinPosWidth() {
+	return getMinPosWidth(first);
+    }
+
+    int selfMinIndent() {
+	return getMinIndent(first);
+    }
+
+    boolean self_contains_brks;
+    boolean self_contains_brks_init = false;
+
+    boolean selfContainsBreaks() {
+	if (self_contains_brks_init) {
+	    return self_contains_brks;
+	} else {
+	    return (self_contains_brks = containsBreaks(first));
+	}
+    }
     String selfToString() {
-	return "[" + first.toString() + "]";
+	if (indent == 0) return "[" + first.toString() + "]";
+	else return "[" + indent + first.toString() + "]";
     }
 }
 
@@ -349,19 +482,29 @@ class StringItem extends Item {
     String s;
     StringItem(String s_) { s = s_; }
         
-    int format1(int lmargin, int pos, int rmargin, int fin, boolean can_break,
-		boolean nofail) 
-        throws Overrun {
-        pos += s.length();
-        if (pos > fin && !nofail) throw new Overrun(pos - fin);
-        return pos;
+    int formatN(int lmargin, int pos, int rmargin, int fin, boolean can_break,
+		boolean nofail) throws Overrun {
+	return format(next, lmargin, pos + s.length(), rmargin, fin,
+		      can_break, nofail);
     }
     int sendOutput(Writer o, int lm, int pos) throws IOException {
         o.write(s);
         return pos + s.length();
     }
     void appendString(String s) { this.s = this.s + s; }
-    String selfToString() { return s; }
+    boolean selfContainsBreaks() { return false; }
+    int selfMinIndent() { return s.length(); }
+    int selfMinWidth() { return s.length(); }
+    int selfMinPosWidth() { return s.length(); }
+    String selfToString() {
+	java.io.StringWriter sw = new java.io.StringWriter();
+	for (int i = 0; i < s.length(); i++) {
+	    char c = s.charAt(i);
+	    if (c == ' ') sw.write("\\ ");
+	    else sw.write(c);
+	}
+	return sw.toString();
+    }
 }
 
 class AllowBreak extends Item 
@@ -372,20 +515,11 @@ class AllowBreak extends Item
         
     AllowBreak(int n_, String alt_) { indent = n_; alt = alt_; }
         
-    int format1(int lmargin, int pos, int rmargin, int fin, boolean can_break,
-		boolean nofail) 
-        throws Overrun {
-	int ret;
-        if (can_break || nofail) {
-            broken = true;
-	    ret = lmargin + indent;
-        } else {
-            broken = false;
-	    ret = pos + alt.length();
-        }
-	if (ret > fin && !nofail)
-	    throw new Overrun(ret - fin);
-	return ret;
+    int formatN(int lmargin, int pos, int rmargin, int fin, boolean can_break,
+    		boolean nofail) throws Overrun {
+        if (can_break) { pos = lmargin + indent; broken = true; }
+        else { pos += alt.length(); broken = false; }
+	return format(next, lmargin, pos, rmargin, fin, can_break, nofail);
     }
         
     int sendOutput(Writer o, int lmargin, int pos)
@@ -399,20 +533,26 @@ class AllowBreak extends Item
             return pos + alt.length();
         }
     }
+    int selfMinIndent() { return indent; }
+    int selfMinPosWidth() { return 0; }
+    int selfMinWidth() { return indent; }
+    boolean selfContainsBreaks() { return true; }
 
-    String selfToString() { return "^"; }
+    String selfToString() {
+	if (indent == 0) return " ";
+	else return "^" + indent; }
 }
 
 class Newline extends AllowBreak 
 {
     Newline(int n_) { super(n_, ""); }
         
-    int format1(int lmargin, int pos, int rmargin, int fin, boolean can_break,
-		boolean nofail) 
-        throws Overrun {
-        if (!can_break) throw new Overrun(1);
+    int formatN(int lmargin, int pos, int rmargin, int fin, boolean can_break,
+		boolean nofail) throws Overrun {
         broken = true;
-        return lmargin + indent;
+	if (!can_break) throw new Overrun(1);
+	return format(next, lmargin, lmargin + indent, rmargin, fin,
+			can_break, nofail);
     }
         
     int sendOutput(Writer o, int lmargin, int pos)
