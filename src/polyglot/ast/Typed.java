1c4d6cdb259655cc5f9157adac559ba5a1ad6075
Michael Clarkson <clarkson@cs.cornell.edu>
2001-06-19 18:33:44 +0000

    Oops -- file missing from previous commit.

---
+++ src/polyglot/ast/Typed.java
@@ -0,0 +1,16 @@
+package jltools.ast;
+
+import jltools.types.*;
+
+/**
+ * <code>Typed</code> represents any node that has a type
+ * associated with it.
+ */
+public interface Typed
+{
+    /**
+     * Return the type of this node, or null if no type has been
+     * assigned yet.
+     */
+    Type type();
+}
