55cf6383ad6d96e6e10de240d1f4ffdb687fd83c
Nate Nystrom <nystrom@cs.cornell.edu>
2001-09-09 20:42:01 +0000

    Fixed type checking of ternary ?: expressions.

--- src/polyglot/ast/Conditional_c.java
+++ src/polyglot/ast/Conditional_c.java
@@ -94,9 +94,91 @@ public class Conditional_c extends Expr_c implements Conditional
 		 cond.position());
 	}
 
-	// FIXME: The rule is more complicated than this.
-	return type(ts.leastCommonAncestor(consequent.type(),
-	    				   alternative.type()));
+	Expr e1 = consequent;
+	Expr e2 = alternative;
+	Type t1 = e1.type();
+	Type t2 = e2.type();
+
+	// From the JLS, section:
+	// If the second and third operands have the same type (which may be
+	// the null type), then that is the type of the conditional expression.
+	if (ts.isSame(t1, t2)) {
+	    return type(t1);
+	}
+
+	// Otherwise, if the second and third operands have numeric type, then
+	// there are several cases:
+	if (t1.isNumeric() && t2.isNumeric()) {
+	    // - If one of the operands is of type byte and the other is of
+	    // type short, then the type of the conditional expression is
+	    // short.
+	    if (t1.isByte() && t2.isShort() || t1.isShort() && t2.isByte()) {
+	        return type(ts.Short());
+	    }
+
+	    // - If one of the operands is of type T where T is byte, short, or
+	    // char, and the other operand is a constant expression of type int
+	    // whose value is representable in type T, then the type of the
+	    // conditional expression is T.
+
+	    Type t = null;
+	    long lit = 0;
+
+	    if (e1 instanceof IntLit) {
+	      	t = t2;
+		lit = ((IntLit) e1).value();
+	    }
+	    else if (e2 instanceof IntLit) {
+	      	t = t1;
+		lit = ((IntLit) e2).value();
+	    }
+
+	    if (t != null) {
+		if (t.isByte() &&
+		    Byte.MIN_VALUE <= lit && lit <= Byte.MAX_VALUE) {
+		    return type(ts.Byte());
+		}
+		if (t.isShort() &&
+		    Short.MIN_VALUE <= lit && lit <= Short.MAX_VALUE) {
+		    return type(ts.Short());
+		}
+		if (t.isChar() &&
+		    Character.MIN_VALUE <= lit && lit <= Character.MAX_VALUE) {
+		    return type(ts.Char());
+		}
+	    }
+
+	    // - Otherwise, binary numeric promotion (§5.6.2) is applied to the
+	    // operand types, and the type of the conditional expression is the
+	    // promoted type of the second and third operands. Note that binary
+	    // numeric promotion performs value set conversion (§5.1.8).
+	    return type(ts.promote(t1, t2));
+	}
+
+	// If one of the second and third operands is of the null type and the
+	// type of the other is a reference type, then the type of the
+	// conditional expression is that reference type.
+	if (t1.isNull() && t2.isReference()) return type(t2);
+	if (t2.isNull() && t1.isReference()) return type(t1);
+
+	// If the second and third operands are of different reference types,
+	// then it must be possible to convert one of the types to the other
+	// type (call this latter type T) by assignment conversion (§5.2); the
+	// type of the conditional expression is T. It is a compile-time error
+	// if neither type is assignment compatible with the other type.
+
+	if (t1.isReference() && t2.isReference()) {
+	    if (t1.isAssignableSubtype(t2)) {
+		return type(t2);
+	    }
+	    if (t2.isAssignableSubtype(t1)) {
+		return type(t1);
+	    }
+	}
+
+	throw new SemanticException(
+	    "Could not find a type for ternary conditional expression.",
+	    position());
     }
 
     public String toString() {
