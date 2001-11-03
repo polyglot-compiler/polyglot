2887882037b307ec81aaba846bd33ffc8349efd2
Jed Liu <liujed@cs.cornell.edu>
2001-11-03 20:12:33 +0000

    Added a visitor for flattening code blocks in the AST.

---
+++ src/polyglot/visit/BlockFlattener.java
@@ -0,0 +1,63 @@
+package jltools.visit;
+
+import jltools.ast.*;
+import jltools.frontend.*;
+import jltools.visit.*;
+import java.util.*;
+
+/**
+ * The <code>BlockFlattener</code> runs over the AST and, as the name
+ * suggests, flattens blocks whenever possible.
+ **/
+public class BlockFlattener extends SemanticVisitor {
+
+  NodeFactory nf;
+
+  /**
+   * Creates a visitor for flattening blocks.
+   *
+   * @param job  The job in which this visitor is being executed.
+   **/
+  public BlockFlattener(Job job) {
+    super(job);
+
+    nf = nodeFactory();
+  }
+
+  public Node leave( Node old, Node n, NodeVisitor v ) {
+    if ( !(n instanceof Block || n instanceof Labeled) ) {
+      return n;
+    }
+
+    // If we have a labeled block consisting of just one statement, then
+    // flatten the block and label the statement instead.
+    if ( n instanceof Labeled ) {
+      Labeled l = (Labeled)n;
+      if ( !(l.statement() instanceof Block) ) {
+        return n;
+      }
+
+      Block b = (Block)l.statement();
+      if ( b.statements().size() != 1 ) {
+        return n;
+      }
+
+      return nf.Labeled( l.position(), l.label(),
+                         (Stmt)b.statements().get(0) );
+    }
+
+    // Flatten any blocks that may be contained in this one.
+    Block b = (Block)n;
+    List stmtList = new LinkedList();
+    for ( Iterator it = b.statements().iterator(); it.hasNext(); ) {
+      Stmt stmt = (Stmt)it.next();
+      if ( stmt instanceof Block ) {
+        stmtList.addAll( ((Block)stmt).statements() );
+      } else {
+        stmtList.add( stmt );
+      }
+    }
+
+    return nf.Block( b.position(), stmtList );
+  }
+}
