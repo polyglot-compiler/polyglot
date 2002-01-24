a566e561ff9d932ac7754284b7ed89121d55f768
Nate Nystrom <nystrom@cs.cornell.edu>
2002-01-24 21:37:49 +0000

    Removed unnecessary disambiguateOverride method.

--- src/polyglot/ast/AmbReceiver_c.java
+++ src/polyglot/ast/AmbReceiver_c.java
@@ -32,27 +32,7 @@ public class AmbReceiver_c extends AmbPrefix_c implements AmbReceiver
     }
 
     /** Disambiguate the receiver. */
-    public Node disambiguateOverride_(AmbiguityRemover ar)
-	throws SemanticException {
-
-	Node n = super.disambiguateOverride_(ar);
-
-	if (n == null) {
-	    return null;
-	}
-
-	if (n instanceof Receiver) {
-	    return n;
-	}
-
-	throw new SemanticException("Could not find type, field, or " +
-	    "local variable \"" + name + "\".", position());
-    }
-
-    /** Disambiguate the receiver. */
-    public Node disambiguate_(AmbiguityRemover ar)
-	throws SemanticException {
-
+    public Node disambiguate_(AmbiguityRemover ar) throws SemanticException {
 	Node n = super.disambiguate_(ar);
 
 	if (n instanceof Receiver) {
