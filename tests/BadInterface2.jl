2fcd147bae79aa5f3ecff794337a575dcd203186
Nate Nystrom <nystrom@cs.cornell.edu>
2002-02-12 22:05:34 +0000

    More test cases.

---
+++ tests/BadInterface2.jl
@@ -0,0 +1,8 @@
+class C0 implements I0
+{
+  protected I0 m0 ()
+  {
+    new I0 ().m0 ();
+      return null;
+  }
+}
---
+++ tests/FieldAccess.jl
@@ -0,0 +1,7 @@
+interface I0
+{
+  public static final I0 f0 = C0.f0.f0.f0.f0.f0.f0;
+  public static class C0 implements I0
+  {
+  }
+}
