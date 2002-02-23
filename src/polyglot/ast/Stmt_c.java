daf9df1822a3d486c2a7bbe2865b6f41ce5b6bd7
Nate Nystrom <nystrom@cs.cornell.edu>
2002-02-23 19:23:27 +0000

    Made Stmt_c abstract.

--- src/polyglot/ast/Stmt_c.java
+++ src/polyglot/ast/Stmt_c.java
@@ -8,7 +8,7 @@ import jltools.util.*;
  * A <code>Stmt</code> represents any Java statement.  All statements must
  * be subtypes of Statement.
  */
-public class Stmt_c extends Node_c implements Stmt
+public abstract class Stmt_c extends Node_c implements Stmt
 {
     public Stmt_c(Ext ext, Position pos) {
 	super(ext, pos);
