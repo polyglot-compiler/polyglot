8df7bb3e39f21954c81eca5ac4bbefa1b3e894e7
Nate Nystrom <nystrom@cs.cornell.edu>
2002-03-11 22:13:08 +0000

    More missing files.

---
+++ src/polyglot/ast/Del.java
@@ -0,0 +1,25 @@
+package jltools.ast;
+
+import jltools.util.Mix;
+import jltools.util.CodeWriter;
+import jltools.util.Copy;
+import jltools.types.SemanticException;
+import jltools.types.TypeSystem;
+import jltools.types.Context;
+
+/**
+ * <code>Ext</code> is the super type of all node extension objects.
+ * It declares the methods which implement compiler passes.
+ */
+public interface Del extends NodeOps, Copy
+{
+    /**
+     * The node which we are ultimately extending, or this.
+     */
+    Node node();
+
+    /**
+     * Initialize the Ext with a Node.
+     */
+    void init(Node node);
+}
