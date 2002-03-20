02d354eb8793bb567308e4dcb1a8f44c8e406875
Nate Nystrom <nystrom@cs.cornell.edu>
2002-03-20 17:00:37 +0000

    Missing file.

---
+++ src/polyglot/visit/HaltingVisitor.java
@@ -0,0 +1,86 @@
+package jltools.visit;
+
+import jltools.ast.Node;
+import jltools.util.*;
+import java.util.*;
+
+public abstract class HaltingVisitor extends NodeVisitor implements Copy
+{
+    Node bypassParent;
+    Collection bypass;
+
+    public HaltingVisitor bypassChildren(Node n) {
+        HaltingVisitor v = (HaltingVisitor) copy();
+        v.bypassParent = n;
+        return v;
+    }
+
+    public HaltingVisitor bypass(Node n) {
+        if (n == null) return this;
+
+        HaltingVisitor v = (HaltingVisitor) copy();
+
+        // FIXME: Using a collection is expensive, but is hopefully not
+        // often used.
+        if (this.bypass == null) {
+            v.bypass = Collections.singleton(n);
+        }
+        else {
+            v.bypass = new ArrayList(this.bypass.size()+1);
+            v.bypass.addAll(bypass);
+            v.bypass.add(n);
+        }
+
+        return v;
+    }
+
+    public HaltingVisitor bypass(Collection c) {
+        if (c == null) return this;
+
+        HaltingVisitor v = (HaltingVisitor) copy();
+
+        // FIXME: Using a collection is expensive, but is hopefully not
+        // often used.
+        if (this.bypass == null) {
+            v.bypass = new ArrayList(c);
+        }
+        else {
+            v.bypass = new ArrayList(this.bypass.size()+c.size());
+            v.bypass.addAll(bypass);
+            v.bypass.addAll(c);
+        }
+
+        return v;
+    }
+
+    public final Node override(Node parent, Node n) {
+        if (bypassParent != null && bypassParent == parent) {
+            // System.out.println("bypassing " + n +
+            //                    " (child of " + parent + ")");
+            return n;
+        }
+
+        if (bypass != null) {
+            for (Iterator i = bypass.iterator(); i.hasNext(); ) {
+                if (i.next() == n) {
+                    // System.out.println("bypassing " + n);
+                    return n;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public Object copy() {
+        try {
+            HaltingVisitor v = (HaltingVisitor) super.clone();
+            v.bypassParent = null;
+            v.bypass = null;
+            return v;
+        }
+        catch (CloneNotSupportedException e) {
+            throw new InternalCompilerError("Java clone() weirdness.");
+        }
+    }
+}
