afa0151ee6b06cfb23243037fff3ed878e3b3460
Nate Nystrom <nystrom@cs.cornell.edu>
2002-07-17 20:07:56 +0000

    Utility class for inserting objects in hashtables using pointer equality.

---
+++ src/polyglot/util/IdentityKey.java
@@ -0,0 +1,27 @@
+package polyglot.util;
+
+/**
+ * Class to be used for inserting objects in hashtables using pointer equality.
+ */
+public class IdentityKey
+{
+    Object obj;
+
+    public IdentityKey(Object obj) {
+        this.obj = obj;
+    }
+
+    public Object object() {
+        return obj;
+    }
+
+    public int hashCode() {
+        return System.identityHashCode(obj);
+    }
+
+    public boolean equals(Object other) {
+        return other instanceof IdentityKey
+            && ((IdentityKey) other).obj == obj;
+    }
+}
+
