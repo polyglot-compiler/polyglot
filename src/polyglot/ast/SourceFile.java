47d39691c5bb3ab4398a4701b3cd757cdf8cfdf9
Jed Liu <liujed@cs.cornell.edu>
2002-08-09 05:01:04 +0000

    Fixed a typo.

--- src/polyglot/ast/SourceFile.java
+++ src/polyglot/ast/SourceFile.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 /**
  * A <code>SourceFile</code> is an immutable representations of a Java
- * langauge source file.  It consists of a package name, a list of 
+ * language source file.  It consists of a package name, a list of 
  * <code>Import</code>s, and a list of <code>GlobalDecl</code>s.
  */
 public interface SourceFile extends Node
