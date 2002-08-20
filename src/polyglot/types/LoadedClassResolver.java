38eae6165ba6f45d5323e37c354aa792cb02dbb6
Nate Nystrom <nystrom@cs.cornell.edu>
2002-08-20 15:14:42 +0000

    Be more forgiving of deserialization failures: if the encoded class is out-of-date fall back on the source file.

--- src/polyglot/types/LoadedClassResolver.java
+++ src/polyglot/types/LoadedClassResolver.java
@@ -28,6 +28,10 @@ public class LoadedClassResolver extends ClassResolver
   Version version;
   Set nocache;
 
+  final static String[] report_topics = new String[] {
+    Report.types, Report.resolver, Report.loader
+  };
+
   /**
    * Create a loaded class resolver.
    * @param ts The type system
@@ -35,7 +39,8 @@ public class LoadedClassResolver extends ClassResolver
    * @param loader The class file loader to use.
    * @param version The version of classes to load.
    */
-  public LoadedClassResolver(TypeSystem ts, String classpath, ClassFileLoader loader, Version version)
+  public LoadedClassResolver(TypeSystem ts, String classpath,
+                             ClassFileLoader loader, Version version)
   {
     this.ts = ts;
     this.te = new TypeEncoder(ts);
@@ -52,20 +57,20 @@ public class LoadedClassResolver extends ClassResolver
       try {
         ClassFile clazz = loader.loadClass(name);
 
-        if (Report.should_report(new String[] {Report.types, Report.resolver, Report.loader}, 4))
-	    Report.report(4, "Class " + name + " found in classpath " +
-                    loader.classpath());
+        if (Report.should_report(report_topics, 4))
+          Report.report(4, "Class " + name + " found in classpath " +
+                        loader.classpath());
 
         return clazz;
       }
       catch (ClassNotFoundException e) {
-        if (Report.should_report(new String[] {Report.types, Report.resolver, Report.loader}, 4))
-	    Report.report(4, "Class " + name + " not found in classpath " +
-                    loader.classpath());
+        if (Report.should_report(report_topics, 4))
+          Report.report(4, "Class " + name + " not found in classpath " +
+                        loader.classpath());
       }
       catch (ClassFormatError e) {
-        if (Report.should_report(new String[] {Report.types, Report.resolver, Report.loader}, 4))
-	    Report.report(4, "Class " + name + " format error");
+        if (Report.should_report(report_topics, 4))
+          Report.report(4, "Class " + name + " format error");
       }
     }
 
@@ -78,20 +83,20 @@ public class LoadedClassResolver extends ClassResolver
    * Find a type by name.
    */
   public Type findType(String name) throws SemanticException {
-    if (Report.should_report(new String[] {Report.types, Report.resolver, Report.loader}, 3))
-	Report.report(3, "LoadedCR.findType(" + name + ")");
+    if (Report.should_report(report_topics, 3))
+      Report.report(3, "LoadedCR.findType(" + name + ")");
 
     // First try the class file.
     ClassFile clazz = loadFile(name);
 
     // Check for encoded type information.
     if (clazz.encodedClassType(version.name()) != null) {
-      if (Report.should_report(new String[] {Report.types, Report.resolver, Report.loader}, 4))
+      if (Report.should_report(report_topics, 4))
 	Report.report(4, "Using encoded class type for " + name);
       return getEncodedType(clazz, name);
     }
     else {
-      if (Report.should_report(new String[] {Report.types, Report.resolver, Report.loader}, 4))
+      if (Report.should_report(report_topics, 4))
 	Report.report(4, "Using raw class file for " + name);
       return clazz.type(ts);
     }
@@ -105,47 +110,42 @@ public class LoadedClassResolver extends ClassResolver
   {
     // At this point we've decided to go with the Class. So if something
     // goes wrong here, we have only one choice, to throw an exception.
+
+    // Check to see if it has serialized info. If so then check the
+    // version.
+    FieldInstance field;
+    
+    int comp = checkCompilerVersion(clazz.compilerVersion(version.name()));
+
+    if (comp == NOT_COMPATIBLE) {
+      throw new SemanticException("Unable to find a suitable definition of "
+                                  + clazz.name()
+                                  + ". Try recompiling or obtaining "
+                                  + " a newer version of the class file.");
+    }
+
+    // Alright, go with it!
+    ClassType dt;
+    
     try {
-      // Check to see if it has serialized info. If so then check the
-      // version.
-      FieldInstance field;
-      
-      int comp = checkCompilerVersion(clazz.compilerVersion(version.name()));
-
-      if (comp == NOT_COMPATIBLE) {
-        throw new SemanticException("Unable to find a suitable definition of "
-                                    + clazz.name()
-                                    + ". Try recompiling or obtaining "
-                                    + " a newer version of the class file.");
-      }
-
-      // Alright, go with it!
-      ClassType dt = (ClassType) te.decode(clazz.encodedClassType(version.name()));
-
-      //HACK: storing median result to avoid circular resolving
-
-      ((CachingResolver) ts.systemResolver()).medianResult(name, dt);
-
-      if (Report.should_report(new String[] {Report.types, Report.resolver, Report.loader}, 2))
-	Report.report(2, "Returning serialized ClassType for " + clazz.name() + ".");
-
-      return (ClassType) dt;
-    }
-    catch (SemanticException e) {
-      // e.printStackTrace();
-      throw e;
-    }
-    catch (RuntimeException e) {
-      // e.printStackTrace();
-      throw e;
+      dt = (ClassType) te.decode(clazz.encodedClassType(version.name()));
     }
     catch (Exception e) {
-      // e.printStackTrace();
       throw new SemanticException("Could not get type information for " +
                                   "class \"" + clazz.name() + "\"; " +
                                   e.getClass().getName() + ": " +
                                   e.getMessage());
     }
+
+    // HACK: storing median result to avoid circular resolving
+
+    ((CachingResolver) ts.systemResolver()).medianResult(name, dt);
+
+    if (Report.should_report(report_topics, 2))
+      Report.report(2, "Returning serialized ClassType for " +
+                    clazz.name() + ".");
+
+    return (ClassType) dt;
   }
 
   /**
--- src/polyglot/types/SourceClassResolver.java
+++ src/polyglot/types/SourceClassResolver.java
@@ -103,7 +103,7 @@ public class SourceClassResolver extends LoadedClassResolver
    * Find a type by name.
    */
   public Type findType(String name) throws SemanticException {
-    if (Report.should_report(new String[] {Report.types, Report.resolver}, 3))
+    if (Report.should_report(report_topics, 3))
 	Report.report(3, "SourceCR.findType(" + name + ")");
 
     ClassFile clazz = null;
@@ -116,8 +116,8 @@ public class SourceClassResolver extends LoadedClassResolver
 
       // Check for encoded type information.
       if (clazz.encodedClassType(version.name()) != null) {
-        if (Report.should_report(new String[] {Report.types, Report.resolver}, 4))
-	    Report.report(4, "Class " + name + " has encoded type info");
+        if (Report.should_report(report_topics, 4))
+          Report.report(4, "Class " + name + " has encoded type info");
         encodedClazz = clazz;
       }
     }
@@ -128,18 +128,18 @@ public class SourceClassResolver extends LoadedClassResolver
     // Now, try and find the source file.
     try {
       source = ext.sourceLoader().classSource(name);
-      if (Report.should_report(new String[] {Report.types, Report.resolver}, 4))
+      if (Report.should_report(report_topics, 4))
 	Report.report(4, "Class " + name + " found in source " + source);
     }
     catch (IOException e) {
-      if (Report.should_report(new String[] {Report.types, Report.resolver}, 4))
+      if (Report.should_report(report_topics, 4))
 	Report.report(4, "Class " + name + " not found in source file");
       source = null;
     }
 
     // Don't use the raw class if the source or encoded class is available.
     if (encodedClazz != null || source != null) {
-      if (Report.should_report(new String[] {Report.types, Report.resolver}, 4))
+      if (Report.should_report(report_topics, 4))
 	Report.report(4, "Not using raw class file for " + name);
       clazz = null;
     }
@@ -153,39 +153,42 @@ public class SourceClassResolver extends LoadedClassResolver
       int comp = checkCompilerVersion(encodedClazz.compilerVersion(version.name()));
 
       if (classModTime < sourceModTime) {
-        if (Report.should_report(new String[] {Report.types, Report.resolver}, 3))
+        if (Report.should_report(report_topics, 3))
 	    Report.report(3, "Source file version is newer than compiled for " +
                       name + ".");
         encodedClazz = null;
       }
       else if (comp != COMPATIBLE) {
         // Incompatible or older version, so go with the source.
-        if (Report.should_report(new String[] {Report.types, Report.resolver}, 3))
+        if (Report.should_report(report_topics, 3))
 	    Report.report(3, "Incompatible source file version for " + name + ".");
         encodedClazz = null;
       }
-      else {
-        source = null;
+    }
+
+    if (encodedClazz != null) {
+      if (Report.should_report(report_topics, 4))
+	Report.report(4, "Using encoded class type for " + name);
+      try {
+        return getEncodedType(encodedClazz, name);
+      }
+      catch (SemanticException e) {
+        if (Report.should_report(report_topics, 4))
+          Report.report(4, "Could not load encoded class " + name);
+        encodedClazz = null;
       }
     }
 
-    // At this point, at most one of clazz, encodedClazz, and source
-    // should be set.
+    // At this point, at most one of clazz and source should be set.
 
     if (clazz != null) {
-      if (Report.should_report(new String[] {Report.types, Report.resolver}, 4))
+      if (Report.should_report(report_topics, 4))
 	Report.report(4, "Using raw class file for " + name);
       return clazz.type(ts);
     }
 
-    if (encodedClazz != null) {
-      if (Report.should_report(new String[] {Report.types, Report.resolver}, 4))
-	Report.report(4, "Using encoded class type for " + name);
-      return getEncodedType(encodedClazz, name);
-    }
-
     if (source != null) {
-      if (Report.should_report(new String[] {Report.types, Report.resolver}, 4))
+      if (Report.should_report(report_topics, 4))
 	Report.report(4, "Using source file for " + name);
       return getTypeFromSource(source, name);
     }
