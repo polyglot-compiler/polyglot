c6fbd431582b2a99ffec628c1b2c30dc89725803
Nate Nystrom <nystrom@cs.cornell.edu>
2002-09-06 07:17:59 +0000

    Forgot to check in.

--- src/polyglot/ext/param/Topics.java
+++ src/polyglot/ext/param/Topics.java
@@ -7,8 +7,10 @@ import polyglot.main.Report;
  */
 public class Topics {
     public static final String param = "param";
+    public static final String subst = "subst";
 
     static {
         Report.topics.add(param);
+        Report.topics.add(subst);
     }
 }
