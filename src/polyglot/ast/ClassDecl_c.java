6d1258f4585e9794fe0a394f519985e38ac5dfaa
Stephen Chong <schong@cs.cornell.edu>
2003-06-25 15:33:50 +0000

    Make the default constructor not have an explicit call to super. The default constructor now has an empty body.

--- src/polyglot/ast/ClassDecl_c.java
+++ src/polyglot/ast/ClassDecl_c.java
@@ -253,9 +253,7 @@ public class ClassDecl_c extends Node_c implements ClassDecl
         ConstructorDecl cd = nf.ConstructorDecl(position(), Flags.PUBLIC,
                                                 name, Collections.EMPTY_LIST,
                                                 Collections.EMPTY_LIST,
-                                                nf.Block(position(),
-                                                nf.SuperCall(position(),
-                                                Collections.EMPTY_LIST)));
+                                                nf.Block(position(),nf.Empty(position())));
         cd = (ConstructorDecl) cd.constructorInstance(ci);
         return body(body.addMember(cd));
     }
