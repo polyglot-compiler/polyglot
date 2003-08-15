dcbd326ef5d93deb74e6b6bafd52ad4eb5905e70
Nate Nystrom <nystrom@cs.cornell.edu>
2003-08-15 15:14:12 +0000

    Added CircularConstructor.jl to makefile.

--- tests/CircularConstructor.jl
+++ tests/CircularConstructor.jl
@@ -1,4 +1,4 @@
-// I believe that this test case should be illegal, based on the JLC 2nd ed,
+// I believe that this test case should be illegal, based on the JLS 2nd ed,
 // Section 8.8.5: "It is a compile-time error for a constructor to directly
 // or indirectly invoke itself through a series of one or more explicit
 // constructor invocations involving <this>."
@@ -6,6 +6,9 @@
 // javac does not catch this, although it does catch a constructor that
 // calls itself, e.g.
 //    CircularConstructor(String s) { this(s); }
+//
+// Correction: javac 1.4.1_01, at least, does catch this
+//
 class CircularConstructor {
     CircularConstructor(String s) {
 	this(5);
