10dc24c4e1cbf30153abfbea615286fc2f9ac428
Stephen Chong <schong@cs.cornell.edu>
2003-08-15 13:32:10 +0000

    Test case with circular constructors, that should be kicked out by Polyglot and javac (but isn't)

---
+++ tests/CircularConstructor.jl
@@ -0,0 +1,19 @@
+// I believe that this test case should be illegal, based on the JLC 2nd ed,
+// Section 8.8.5: "It is a compile-time error for a constructor to directly
+// or indirectly invoke itself through a series of one or more explicit
+// constructor invocations involving <this>."
+//
+// javac does not catch this, although it does catch a constructor that
+// calls itself, e.g.
+//    CircularConstructor(String s) { this(s); }
+class CircularConstructor {
+    CircularConstructor(String s) {
+	this(5);
+    }
+    CircularConstructor(int i) {
+	this("s");
+    }
+    public static void main(String[] args) {
+	new CircularConstructor("df");
+    }
+}
