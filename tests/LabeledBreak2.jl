410f2f98eac6ffc6832b9d02a0c05cf421507593
Nate Nystrom <nystrom@cs.cornell.edu>
2003-08-30 02:33:51 +0000

    Expect LabeledBreak2 to fail.  It does not compile with Polyglot. It does compile with javac, but the JLS is ambiguous. Added a local class test case.

--- tests/LabeledBreak2.jl
+++ tests/LabeledBreak2.jl
@@ -1,6 +1,8 @@
 class LabeledBreak {
     public static void main(String[] args) {
         int i;
+        // Polyglot says ++i is unreachable; javac says it's reachable,
+        // but the JLS doesn't say which is correct.
         a: for (i=0; i<10; ++i) {
             break a;
         }
