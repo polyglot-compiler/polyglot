1bdade2b31aded2a22f34e6ab6bc6051dcd02df2
Stephen Chong <schong@cs.cornell.edu>
2003-11-13 15:28:39 +0000

    Cache the first level of contents of source path directories. This permits a much more efficient checking of whether a given file exists in that source path, and avoids a lot of calls to File.exists(), which (because it needs to go to the file system) is expensive.

--- src/polyglot/frontend/SourceLoader.java
+++ src/polyglot/frontend/SourceLoader.java
@@ -9,10 +9,18 @@ public class SourceLoader
 {
     ExtensionInfo sourceExt;
     Collection sourcePath;
+    
+    /**
+     * This is a map from Files (of directories) to Set[String]s, which
+     * records the first level of contents of the directory. This cache
+     * is used to avoid a number of File.exists() calls.
+     */
+    Map directoryContentsCache;
 
     public SourceLoader(ExtensionInfo sourceExt, Collection sourcePath) {
 	this.sourcePath = sourcePath;
 	this.sourceExt = sourceExt;
+        this.directoryContentsCache = new HashMap();
     }
 
     /** Load a source from a specific file. */
@@ -53,22 +61,42 @@ public class SourceLoader
 
 	for (Iterator i = sourcePath.iterator(); i.hasNext(); ) {
 	    File directory = (File) i.next();
+            Set dirContents = (Set)directoryContentsCache.get(directory);
+            if (dirContents == null) {
+                dirContents = new HashSet();
+                directoryContentsCache.put(directory, dirContents);
+                if (directory.exists()) {
+                    String[] contents = directory.list();
+                    for (int j = 0; j < contents.length; j++) {
+                        dirContents.add(contents[j]);
+                    }
+                }                
+            }
 
-	    File sourceFile;
+            // check if the source file exists in the directory
+            int index = fileName.indexOf(File.separatorChar);
+            if (index < 0) index = fileName.length(); 
+            String firstPart = fileName.substring(0, index);
 
-	    if (directory != null && directory.equals(current_dir)) {
-		sourceFile = new File(fileName);
-	    }
-	    else {
-	        sourceFile = new File(directory, fileName);
-	    }
-	    
-	    if (sourceFile.exists()) {
-		if (Report.should_report(Report.frontend, 2))
-		    Report.report(2, "Loading " + className + " from " + sourceFile);
-
-		return new FileSource(sourceFile);
-	    }
+            if (dirContents.contains(firstPart)) {
+                // the directory contains at least the first part of the
+                // file path. We will check if this file exists.
+                File sourceFile;
+                
+                if (directory != null && directory.equals(current_dir)) {
+                    sourceFile = new File(fileName);
+                }
+                else {
+                    sourceFile = new File(directory, fileName);
+                }
+                
+                if (sourceFile.exists()) {
+                    if (Report.should_report(Report.frontend, 2))
+                        Report.report(2, "Loading " + className + " from " + sourceFile);
+                
+                    return new FileSource(sourceFile);
+                }
+            }
 	}
 
 	return null;
