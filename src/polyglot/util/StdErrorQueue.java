2c685cef213938fcf331d4500e1eaeffa6051244
Stephen Chong <schong@cs.cornell.edu>
2004-02-28 23:09:04 +0000

    Cleaned up the display of multiline errors.
    Will now print e.g.
            return this.foo().
                   ^----------
                     bar().foo();
                     ----^
    
    If the error goes over more than 2 lines, will print first and last lines of error only, with ellipsis for intervening lines.

--- src/polyglot/util/StdErrorQueue.java
+++ src/polyglot/util/StdErrorQueue.java
@@ -124,50 +124,105 @@ public class StdErrorQueue extends AbstractErrorQueue
         LineNumberReader reader = new LineNumberReader(r);
 
         String s = null;
-        
-        for (int i = 0; i < pos.line(); i++) {
+        while (reader.getLineNumber() < pos.line()) {
           s = reader.readLine();
         }
-        reader.close();
 
         if (s != null) {
           err.println(s);
+          showErrorIndicator(pos, reader.getLineNumber(), s);
+          
+          if (pos.endLine() != pos.line() &&
+              pos.endLine() != Position.UNKNOWN &&
+              pos.endLine() != Position.END_UNUSED) {
 
-          if (pos.column() != Position.UNKNOWN) {
-            for (int i = 0; i < pos.column(); i++) {
-              if (s.charAt(i) == '\t') {
-                err.print("\t");
+              // if there is more than two lines,
+              // print some ellipsis.
+              if (pos.endLine() - pos.line() > 1) {
+                  // add some whitespace first
+                  for (int j = 0; j < s.length() && Character.isWhitespace(s.charAt(j)); j++) {
+                      err.print(s.charAt(j));
+                  }
+                  err.println("...");
               }
-              else {
-                err.print(" ");
+              
+              while (reader.getLineNumber() < pos.endLine()) {
+                s = reader.readLine();
+              }
+
+              // s is now the last line of the error.
+              if (s != null) {
+                err.println(s);
+                showErrorIndicator(pos, reader.getLineNumber(), s);
               }
             }
-
-            if (pos.endLine() == pos.line() && 
-                pos.endColumn() != Position.UNKNOWN &&
-                pos.endColumn() != Position.END_UNUSED &&
-                pos.endColumn() != pos.column() &&
-                pos.endColumn() - pos.column() > 1) {
-                err.print("^");
-                int length = pos.endColumn()-pos.column();
-                for (int i = 0; i < length-2; i++) {
-                  if (s.charAt(i) == '\t') {
-                    err.print("\t");
-                  }
-                  else {
-                    err.print("-");
-                  }
-                }                
-            }
-            
-            err.println("^");
-          }
-
-          err.println();
         }
+        
+        reader.close();
+
+        err.println();
       }
       catch (IOException e) {
       }
+    }
+    
+    protected void showErrorIndicator(Position pos, int lineNum, String s) {
+        if (pos.column() == Position.UNKNOWN) {
+            return;
+        }
+        
+        int i = 0;
+        while (i < s.length() && Character.isWhitespace(s.charAt(i))) {
+            err.print(s.charAt(i++));
+        }
+        
+        int startIndAt = i; // column position to start showing the indicator marks
+        int stopIndAt = s.length() - 1; // column position to stop showing the indicator marks
+        if (pos.line() == lineNum) {
+            startIndAt = pos.column();
+        }        
+        if (pos.endLine() == lineNum) {
+            stopIndAt = pos.endColumn() - 1;
+        }
+        if (pos.endColumn() == Position.UNKNOWN ||
+            pos.endColumn() == Position.END_UNUSED) {
+            stopIndAt = startIndAt;
+        }
+                
+        for (;i <= stopIndAt; i++) {
+            if (i < startIndAt) {
+                if (s.charAt(i) == '\t') {
+                  err.print("\t");
+                }
+                else {
+                  err.print(" ");
+                }
+            }
+            else if (i == startIndAt) {
+                if (pos.line() == lineNum) {
+                  err.print("^");
+                }
+                else {
+                  err.print("-");
+                }
+            }
+            else if (i == stopIndAt) {
+                if (pos.endLine() == lineNum) {
+                  err.print("^");
+                }
+                else {
+                  err.print("-");
+                }
+            }
+            else if (s.charAt(i) == '\t') {
+              err.print("\t");
+            }
+            else {
+              err.print("-");
+            }
+        }
+
+        err.println();
     }
     
     public void flush() {
