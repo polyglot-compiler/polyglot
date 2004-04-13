0ecbbbc916e4556676e7b36eb028518e3f51bc62
Nate Nystrom <nystrom@cs.cornell.edu>
2004-04-13 20:38:44 +0000

    Javadoced Pass.

--- src/polyglot/frontend/Pass.java
+++ src/polyglot/frontend/Pass.java
@@ -2,42 +2,103 @@ package polyglot.frontend;
 
 import polyglot.util.Enum;
 
-/** A <code>Pass</code> represents a compiler pass. */
+/** A <code>Pass</code> represents a compiler pass.
+ * A <code>Job</code> runs a series of passes over the AST. 
+ * Each pass has an ID that is used to identify similar passes across
+ * several jobs.  For example, most jobs contain a pass named PARSE
+ * that returns an AST for a source file and a pass TYPE_CHECK
+ * that performs type checking on the job's AST.
+ */
 public interface Pass
 {
+    /** 
     public static class ID extends Enum {
         public ID(String name) { super(name); }
     }
 
+    /** Return the identifier for the pass.
+     * The identifier should be unique within the pass's Job. 
+     */
     public ID id();
+
+    /** Return a user-readable name for the pass. */
     public String name();
-
-    public static final ID PARSE = new ID("parse");
-    public static final ID BUILD_TYPES = new ID("build-types");
-    public static final ID BUILD_TYPES_ALL = new ID("build-types-barrier");
-    public static final ID CLEAN_SUPER = new ID("clean-super");
-    public static final ID CLEAN_SUPER_ALL = new ID("clean-super-barrier");
-    public static final ID CLEAN_SIGS = new ID("clean-sigs");
-    public static final ID ADD_MEMBERS = new ID("add-members");
-    public static final ID ADD_MEMBERS_ALL = new ID("add-members-barrier");
-    public static final ID DISAM = new ID("disam");
-    public static final ID DISAM_ALL = new ID("disam-barrier");
-    public static final ID TYPE_CHECK = new ID("type-check");
-    public static final ID SET_EXPECTED_TYPES = new ID("set-expected-types");
-    public static final ID EXC_CHECK = new ID("exc-check");
-    public static final ID FOLD = new ID("fold");
-    public static final ID INIT_CHECK = new ID("init-check");
-    public static final ID CONSTRUCTOR_CHECK = new ID("constructor-check");
-    public static final ID FWD_REF_CHECK = new ID("fwd-reference-check");
-    public static final ID REACH_CHECK = new ID("reach-check");
-    public static final ID EXIT_CHECK = new ID("exit-check");
-    public static final ID DUMP = new ID("dump");
-    public static final ID PRE_OUTPUT_ALL = new ID("pre-output-barrier");
-    public static final ID SERIALIZE = new ID("serialization");
-    public static final ID OUTPUT = new ID("output");
-
-    public static final ID FIRST_BARRIER = BUILD_TYPES_ALL;
 
     /** Run the pass. */
     public boolean run();
+
+    // Below are the IDs of all the passes of the base compiler,
+    // plus a few optional passes that extensions may use.
+
+    /** ID of the parser pass.  See Parser. */
+    public static final ID PARSE = new ID("parse");
+
+    /** ID of the build-types pass.  See TypeBuilder. */
+    public static final ID BUILD_TYPES = new ID("build-types");
+
+    /** ID of the barrier after build-types. */
+    public static final ID BUILD_TYPES_ALL = new ID("build-types-barrier");
+
+    /** ID of the clean-super pass.  See AmbiguityRemover. */
+    public static final ID CLEAN_SUPER = new ID("clean-super");
+
+    /** ID of the barrier after clean-super. */
+    public static final ID CLEAN_SUPER_ALL = new ID("clean-super-barrier");
+
+    /** ID of the clean-sigs pass.  See AmbiguityRemover. */
+    public static final ID CLEAN_SIGS = new ID("clean-sigs");
+
+    /** ID of the add-members pass.  See AddMemberVisitor. */
+    public static final ID ADD_MEMBERS = new ID("add-members");
+
+    /** ID of the barrier after add-members. */
+    public static final ID ADD_MEMBERS_ALL = new ID("add-members-barrier");
+
+    /** ID of the disambiguate pass.  See AmbiguityRemover. */
+    public static final ID DISAM = new ID("disam");
+
+    /** ID of the barrier after disam. */
+    public static final ID DISAM_ALL = new ID("disam-barrier");
+
+    /** ID of the type-check pass.  See TypeChecker. */
+    public static final ID TYPE_CHECK = new ID("type-check");
+
+    /** ID of the set-expected-types pass.  See AscriptionVisitor. */
+    public static final ID SET_EXPECTED_TYPES = new ID("set-expected-types");
+
+    /** ID of the exception-check pass.  See ExceptionChecker. */
+    public static final ID EXC_CHECK = new ID("exc-check");
+
+    /** ID of the constant-fold pass.  See ConstantFolder. */
+    public static final ID FOLD = new ID("fold");
+
+    /** ID of the initialization-check pass.  See InitChecker. */
+    public static final ID INIT_CHECK = new ID("init-check");
+
+    /** ID of the constructor-check pass.  See ConstructorChecker. */
+    public static final ID CONSTRUCTOR_CHECK = new ID("constructor-check");
+
+    /** ID of the forward-reference-check pass.  See FwdReferenceChecker. */
+    public static final ID FWD_REF_CHECK = new ID("fwd-reference-check");
+
+    /** ID of the reachability-check pass.  See ReachChecker. */
+    public static final ID REACH_CHECK = new ID("reach-check");
+
+    /** ID of the exit-check pass.  See ExitChecker. */
+    public static final ID EXIT_CHECK = new ID("exit-check");
+
+    /** ID of the AST dumper pass.  See DumpAST. */
+    public static final ID DUMP = new ID("dump");
+
+    /** ID of the barrier before output. */
+    public static final ID PRE_OUTPUT_ALL = new ID("pre-output-barrier");
+
+    /** ID of the class serialzation pass.  See ClassSerializer. */
+    public static final ID SERIALIZE = new ID("serialization");
+
+    /** ID of the output pass.  See OutputPass. */
+    public static final ID OUTPUT = new ID("output");
+
+    /** ID of the first barrier pass. */
+    public static final ID FIRST_BARRIER = BUILD_TYPES_ALL;
 }
