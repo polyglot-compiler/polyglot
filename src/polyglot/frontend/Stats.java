4f6deb65643f3eedda49bd285f71711de30c1448
Nate Nystrom <nystrom@cs.cornell.edu>
2004-06-25 23:45:54 +0000

    Changed the key for statistics from Pass.ID to Object.  This lets one more easily insert statistics gathering code for debugging performance problems.

--- src/polyglot/frontend/Stats.java
+++ src/polyglot/frontend/Stats.java
@@ -21,27 +21,27 @@ public class Stats
     /** Extension we're collecting stats for. */
     protected ExtensionInfo ext;
 
-    /** Map from Pass.ID to pair of inclusive and exclusive times. */
+    /** Map from Objects to pair of inclusive and exclusive times. */
     protected Map passTimes = new HashMap();
 
     /**
-     * List of Pass.IDs used as keys to passTimes.  We have an explicit
-     * list in order to report the passes in order.
+     * List of Objects used as keys to passTimes.  We have an explicit
+     * list in order to report the keys in order.
      */
-    protected List passes = new ArrayList(20);
+    protected List keys = new ArrayList(20);
 
     public Stats(ExtensionInfo ext) {
         this.ext = ext;
     }
 
     /** Reset the accumulated times for a pass. */
-    public void resetPassTimes(Pass.ID id) {
-        passTimes.remove(id);
+    public void resetPassTimes(Object key) {
+        passTimes.remove(key);
     }
 
     /** Return the accumulated times for a pass. */
-    public long passTime(Pass.ID id, boolean inclusive) {
-        Times t = (Times) passTimes.get(id);
+    public long passTime(Object key, boolean inclusive) {
+        Times t = (Times) passTimes.get(key);
         if (t == null) {
             return 0;
         }
@@ -50,12 +50,12 @@ public class Stats
     }
 
     /** Accumulate inclusive and exclusive times for a pass. */
-    public void accumPassTimes(Pass.ID id, long in, long ex) {
-        Times t = (Times) passTimes.get(id);
+    public void accumPassTimes(Object key, long in, long ex) {
+        Times t = (Times) passTimes.get(key);
         if (t == null) {
-            passes.add(id);
+            keys.add(key);
             t = new Times();
-            passTimes.put(id, t);
+            passTimes.put(key, t);
         }
         t.inclusive += in;
         t.exclusive += ex;
@@ -69,11 +69,11 @@ public class Stats
             Report.report(1, "Pass Inclusive Exclusive");
             Report.report(1, "---- --------- ---------");
 
-            for (Iterator i = passes.iterator(); i.hasNext(); ) {
-                Pass.ID id = (Pass.ID) i.next();
-                Times t = (Times) passTimes.get(id);
+            for (Iterator i = keys.iterator(); i.hasNext(); ) {
+                Object key = i.next();
+                Times t = (Times) passTimes.get(key);
 
-                Report.report(1, id.toString() + " " +
+                Report.report(1, key.toString() + " " +
                                  t.inclusive + " " + t.exclusive);
             }
         }
