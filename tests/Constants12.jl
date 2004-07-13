37cebb1c75d746e23850b04d620f1d1f63bd7613
Nate Nystrom <nystrom@cs.cornell.edu>
2004-07-13 06:09:14 +0000

    *** empty log message ***

---
+++ tests/Constants12.jl
@@ -0,0 +1,13 @@
+public class Constants12 {
+  public static void main(String[] args) {
+    System.out.println(0-Integer.MAX_VALUE);
+
+    System.out.println((int)(-1 << -1));
+    System.out.println((int)(1 << -1));
+    System.out.println((int)(-1 >>> 1));
+
+    System.out.println(0L-Long.MAX_VALUE);
+
+
+   }
+}
