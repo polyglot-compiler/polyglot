0ffec46d99828a2929fd7fa5db61469e25d146f6
Nate Nystrom <nystrom@cs.cornell.edu>
2004-07-14 02:54:42 +0000

    Bug fix: Classes declared in a static context are not inner classes.

--- src/polyglot/types/ClassType_c.java
+++ src/polyglot/types/ClassType_c.java
@@ -99,7 +99,7 @@ public abstract class ClassType_c extends ReferenceType_c implements ClassType
     public boolean isInnerClass() {
         // it's an inner class if it is not an interface, it is a nested
         // class, and it is not explicitly or implicitly static. 
-        return !flags().isInterface() && isNested() && !flags().isStatic();
+        return !flags().isInterface() && isNested() && !flags().isStatic() && !inStaticContext();
     }
     
     public boolean isCanonical() { return true; }
