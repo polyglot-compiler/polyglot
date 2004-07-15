b38eda0b53beedb56150636c97d276554c81c531
Nate Nystrom <nystrom@cs.cornell.edu>
2004-07-15 17:31:55 +0000

    Test folding boundary cases for ints and longs.

--- tests/Constants12.jl
+++ tests/Constants12.jl
@@ -1,13 +1,34 @@
 public class Constants12 {
-  public static void main(String[] args) {
-    System.out.println(0-Integer.MAX_VALUE);
-
-    System.out.println((int)(-1 << -1));
-    System.out.println((int)(1 << -1));
-    System.out.println((int)(-1 >>> 1));
-
-    System.out.println(0L-Long.MAX_VALUE);
-
-
-   }
+  int m(int x) {
+    switch (x) {
+      case (0-Integer.MAX_VALUE):
+        System.out.println("0"); break;
+      case -2147483647:
+        System.out.println("1"); break; // error: duplicate label
+    }
+    switch (x) {
+      case ((int)(-1 << -1)):
+        System.out.println("0"); break;
+      case -2147483648:
+        System.out.println("1"); break; // error: duplicate label
+    }
+    switch (x) {
+      case ((int)(1 << -1)):
+        System.out.println("1"); break;
+      case -2147483648:
+        System.out.println("2"); break; // error: duplicate label
+    }
+    switch (x) {
+      case ((int)(-1 >>> 1)):
+        System.out.println("0"); break;
+      case 2147483647:
+        System.out.println("1"); break; // error: duplicate label
+    }
+    switch (x) {
+      case (int) (0L-Long.MAX_VALUE):
+        System.out.println("0"); break;
+      case (int) -9223372036854775807:
+        System.out.println("1"); break; // error: duplicate label
+    }
+  }
 }
