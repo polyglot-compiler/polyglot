8fbe9219876bf150354c4f7ea9671d1bf207b74c
Nate Nystrom <nystrom@cs.cornell.edu>
2005-07-14 21:27:16 +0000

    Added isDisambiguated method to SourceFile.

--- src/polyglot/ast/SourceFile_c.java
+++ src/polyglot/ast/SourceFile_c.java
@@ -27,6 +27,10 @@ public class SourceFile_c extends Node_c implements SourceFile
 	this.imports = TypedList.copyAndCheck(imports, Import.class, true);
 	this.decls = TypedList.copyAndCheck(decls, TopLevelDecl.class, true);
     }
+    
+    public boolean isDisambiguated() {
+        return super.isDisambiguated() && this.importTable != null;
+    }
 
     /** Get the source of the source file. */
     public Source source() {
