139a29ccf9a15df643d68e6fa3bddabeddade798
Stephen Chong <schong@cs.cornell.edu>
2005-08-31 19:55:30 +0000

    Added a testcase Output.jl which seems to make the code writer go into an infinite loop. Not sure why yet....

---
+++ tests/Output.jl
@@ -0,0 +1,18 @@
+public class Header  {
+
+    private void jsa$html$BlockContainer$(String s, Object n) { }
+
+    public Header jsa$html$Header$(final int level,
+                                            final Object contents) {
+        {
+	    
+	    this.jsa$html$BlockContainer$("h" +
+                                           (level <
+                                              1 ? "1" : (level >
+                                                           5 ? "5" : java.lang.Integer.toString(level))),
+                                           contents);
+
+        }
+        return this;
+    }
+}
