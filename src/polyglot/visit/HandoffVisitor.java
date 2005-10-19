e3a4fd1e6502f56145bd66455bec0d3ef8865f00
Nate Nystrom <nystrom@cs.cornell.edu>
2005-10-19 07:15:47 +0000

    This visitor just adds a job for each SourceFile node in the AST to a given schedule.  Factored out from Jx, but Jif and JMatch do something similar and could use this visitor.

---
+++ src/polyglot/visit/HandoffVisitor.java
@@ -0,0 +1,37 @@
+package polyglot.visit;
+
+import java.util.*;
+
+import polyglot.ast.Node;
+import polyglot.ast.SourceFile;
+import polyglot.ast.SourceCollection;
+import polyglot.frontend.ExtensionInfo;
+import polyglot.visit.NodeVisitor;
+
+/**
+ * This visitor adds jobs for <code>SourceFile</code>s in the AST to the
+ * schedule of another extension.
+ */
+public class HandoffVisitor extends NodeVisitor
+{
+    protected ExtensionInfo ext;
+
+    public HandoffVisitor(ExtensionInfo ext) {
+        this.ext = ext;
+    }
+
+    public Node override(Node n) {
+        if (n instanceof SourceFile || n instanceof SourceCollection) {
+            return null;
+        }
+        return n;
+    }
+
+    public Node leave(Node old, Node n, NodeVisitor v) {
+        if (n instanceof SourceFile) {
+            SourceFile sf = (SourceFile) n;
+            ext.scheduler().addJob(sf.source(), sf);
+        }
+        return n;
+    }
+}
