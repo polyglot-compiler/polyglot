2b62d9cb3d90916bc609f8ca47b4700857adde54
Nate Nystrom <nystrom@cs.cornell.edu>
2005-10-26 18:32:34 +0000

    Reporting in signaturesResolved().

--- src/polyglot/types/ParsedClassType_c.java
+++ src/polyglot/types/ParsedClassType_c.java
@@ -334,7 +334,8 @@ public class ParsedClassType_c extends ClassType_c implements ParsedClassType
             for (Iterator i = l.iterator(); i.hasNext(); ) {
                 MemberInstance mi = (MemberInstance) i.next();
                 if (! mi.isCanonical()) {
-// System.out.println(mi + " is ambiguous");
+                    if (Report.should_report("ambcheck", 2)
+                        Report.report(2, mi + " is ambiguous");
                     count++;
                 }
             }
