660eb35b15a3285c1204e3c8038ea8cf25d6ae2a
Nate Nystrom <nystrom@cs.cornell.edu>
2007-01-04 06:55:02 +0000

    Added build_string() method containing build time.

--- src/polyglot/frontend/JLVersion.java
+++ src/polyglot/frontend/JLVersion.java
@@ -1,22 +1,18 @@
 /*
  * This file is part of the Polyglot extensible compiler framework.
  *
- * Copyright (c) 2000-2006 Polyglot project group, Cornell University
- * 
+ * Copyright (c) 2000-2007 Polyglot project group, Cornell University
+ * Copyright (c) 2006-2007 IBM Corporation
  */
-
 package polyglot.frontend;
 
 /**
  * Version information for the base compiler.
  */
 public class JLVersion extends polyglot.main.Version {
-    public String name()
-        { return "jl"; }
-    public int major()
-	{ return 2; }
-    public int minor()
-	{ return 0; }
-    public int patch_level()
-	{ return 1; }
+    public String name() { return "jl"; }
+    public int major() { return 2; }
+    public int minor() { return 0; }
+    public int patch_level() { return 2; }
+    public String build_string() { return "Polyglot compiler toolkit version 2.0.2"; }
 }
