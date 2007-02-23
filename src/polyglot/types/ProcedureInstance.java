fbce22d2cb455fd03e25995876c2e005a0008615
Nate Nystrom <nystrom@cs.cornell.edu>
2007-02-23 17:00:24 +0000

    Copyright notice.

--- src/polyglot/types/ProcedureInstance.java
+++ src/polyglot/types/ProcedureInstance.java
@@ -2,6 +2,7 @@
  * This file is part of the Polyglot extensible compiler framework.
  *
  * Copyright (c) 2000-2006 Polyglot project group, Cornell University
+ * Copyright (c) 2007 IBM Corporation
  * 
  */
 
