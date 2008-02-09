601823f9a4f2316fd8462f8546a53ab2f3a2b879
Stephen Chong <schong@cs.cornell.edu>
2008-02-09 04:05:52 +0000

    Some missing getter methods.

--- src/polyglot/parse/VarDeclarator.java
+++ src/polyglot/parse/VarDeclarator.java
@@ -14,19 +14,28 @@ import polyglot.util.*;
  * Encapsulates some of the data in a variable declaration.  Used only by the parser.
  */
 public class VarDeclarator {
-	public Position pos;
-	public Id name;
-	public int dims;
-	public Expr init;
+    public Position pos;
+    public Id name;
+    public int dims;
+    public Expr init;
 
-	public VarDeclarator(Position pos, Id name) {
-		this.pos = pos;
-		this.name = name;
-		this.dims = 0;
-		this.init = null;
-	}
-	
-	public Position position() {
-		return pos;
-	}
+    public VarDeclarator(Position pos, Id name) {
+        this.pos = pos;
+        this.name = name;
+        this.dims = 0;
+        this.init = null;
+    }
+
+    public Id name() {
+        return name;
+    }
+    public int dims() {
+        return dims;
+    }
+    public Expr init() {
+        return init;
+    }
+    public Position position() {
+        return pos;
+    }
 }
