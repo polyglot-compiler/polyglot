c5c50fbf773dd77f0c9659dbf3038d0867356c11
Nate Nystrom <nystrom@cs.cornell.edu>
2008-08-14 15:18:14 +0000

    Bump version.

--- src/polyglot/frontend/JLVersion.java
+++ src/polyglot/frontend/JLVersion.java
@@ -12,7 +12,7 @@ package polyglot.frontend;
 public class JLVersion extends polyglot.main.Version {
     public String name() { return "jl"; }
     public int major() { return 2; }
-    public int minor() { return 3; }
+    public int minor() { return 4; }
     public int patch_level() { return 0; }
-    public String toString() { return "2.3.0 (2007-10-02 09:09:53)"; }
+    public String toString() { return "2.4.0 (2008-08-14 11:16:45)"; }
 }
