a0c314f48abc8c3e673877cec97cf34e89b7d583
Stephen Chong <schong@cs.cornell.edu>
2012-03-22 18:37:51 +0000

    Fixed spacing.

--- src/polyglot/ext/jl5/ast/JL5AssignDel.java
+++ src/polyglot/ext/jl5/ast/JL5AssignDel.java
@@ -21,72 +21,73 @@ public class JL5AssignDel extends JL5Del {
 
         if (! (a.left() instanceof Variable)) {
             throw new SemanticException("Target of assignment must be a variable.",
-                    a.position());
+                                        a.position());
         }
 
         if (a.operator() == Assign.ASSIGN) {
-          if (! ts.isImplicitCastValid(s, t) &&
-              ! ts.typeEquals(s, t) &&
-              ! ts.numericConversionValid(t, a.right().constantValue())) {
+            if (! ts.isImplicitCastValid(s, t) &&
+                    ! ts.typeEquals(s, t) &&
+                    ! ts.numericConversionValid(t, a.right().constantValue())) {
 
-            throw new SemanticException("Cannot assign " + s + " to " + t + ".",
-                    a.position());
-          }
+                throw new SemanticException("Cannot assign " + s + " to " + t + ".",
+                                            a.position());
+            }
 
-          return a.type(t);
+            
+            return a.type(t);
         }
 
         if (a.operator() == Assign.ADD_ASSIGN) {
-          // t += s
-          if (ts.typeEquals(t, ts.String()) && ts.canCoerceToString(s, tc.context())) {
-            return a.type(ts.String());
-          }
+            // t += s
+            if (ts.typeEquals(t, ts.String()) && ts.canCoerceToString(s, tc.context())) {
+                return a.type(ts.String());
+            }
 
-          if (isNumeric(t) && isNumeric(s)) {
-            return a.type(ts.promote(numericType(t), numericType(s)));
-          }
+            if (isNumeric(t) && isNumeric(s)) {
+                return a.type(ts.promote(numericType(t), numericType(s)));
+            }
 
-          throw new SemanticException("The " + a.operator() + " operator must have "
-                                      + "numeric or String operands.",
-                                      a.position());
+            throw new SemanticException("The " + a.operator() + " operator must have "
+                                        + "numeric or String operands.",
+                                        a.position());
         }
 
         if (a.operator() == Assign.SUB_ASSIGN || a.operator() == Assign.MUL_ASSIGN ||
                 a.operator() == Assign.DIV_ASSIGN || a.operator() == Assign.MOD_ASSIGN) {
-          if (isNumeric(t) && isNumeric(s)) {
-            return a.type(ts.promote(numericType(t), numericType(s)));
-          }
+            if (isNumeric(t) && isNumeric(s)) {
+                return a.type(ts.promote(numericType(t), numericType(s)));
+            }
 
-          throw new SemanticException("The " + a.operator() + " operator must have "
-                                      + "numeric operands.",
-                                      a.position());
+            throw new SemanticException("The " + a.operator() + " operator must have "
+                                        + "numeric operands.",
+                                        a.position());
         }
 
         if (a.operator() == Assign.BIT_AND_ASSIGN || a.operator() == Assign.BIT_OR_ASSIGN || a.operator() == Assign.BIT_XOR_ASSIGN) {
-          if (isBoolean(t) && isBoolean(s)) {
-            return a.type(ts.Boolean());
-          }
+            if (isBoolean(t) && isBoolean(s)) {
+                return a.type(ts.Boolean());
+            }
 
-          if (ts.isImplicitCastValid(t, ts.Long()) &&
-              ts.isImplicitCastValid(s, ts.Long())) {
-            return a.type(ts.promote(numericType(t), numericType(s)));
-          }
+            if (ts.isImplicitCastValid(t, ts.Long()) &&
+                    ts.isImplicitCastValid(s, ts.Long())) {
+                return a.type(ts.promote(numericType(t), numericType(s)));
+            }
 
-          throw new SemanticException("The " + a.operator() + " operator must have "
-                                      + "integral or boolean operands.",
-                                      a.position());
+            throw new SemanticException("The " + a.operator() + " operator must have "
+                                        + "integral or boolean operands.",
+                                        a.position());
         }
 
         if (a.operator() == Assign.SHL_ASSIGN || a.operator() == Assign.SHR_ASSIGN || a.operator() == Assign.USHR_ASSIGN) {
-          if (ts.isImplicitCastValid(t, ts.Long()) &&
-              ts.isImplicitCastValid(s, ts.Long())) {
-            // Only promote the left of a shift.
-            return a.type(ts.promote(numericType(t)));
-          }
+            if (ts.isImplicitCastValid(t, ts.Long()) &&
+                    ts.isImplicitCastValid(s, ts.Long())) {
+                // Only promote the left of a shift.
+                return a.type(ts.promote(numericType(t)));
+            }
 
-          throw new SemanticException("The " + a.operator() + " operator must have "
-                                      + "integral operands.",
-                                      a.position());
+            throw new SemanticException("The " + a.operator() + " operator must have "
+                                        + "integral operands.",
+                                        a.position());
         }
 
         throw new InternalCompilerError("Unrecognized assignment operator " +
@@ -96,7 +97,7 @@ public class JL5AssignDel extends JL5Del {
     private boolean isNumeric(Type t) {
         if (t.isNumeric()) return true;
         JL5TypeSystem ts = (JL5TypeSystem) t.typeSystem();
-        
+
         if (ts.isPrimitiveWrapper(t)) {
             return ts.primitiveTypeOfWrapper(t).isNumeric();
         }
@@ -106,7 +107,7 @@ public class JL5AssignDel extends JL5Del {
     private boolean isBoolean(Type t) {
         if (t.isBoolean()) return true;
         JL5TypeSystem ts = (JL5TypeSystem) t.typeSystem();
-        
+
         if (ts.isPrimitiveWrapper(t)) {
             return ts.primitiveTypeOfWrapper(t).isBoolean();
         }
@@ -116,7 +117,7 @@ public class JL5AssignDel extends JL5Del {
     private Type numericType(Type t) {
         if (t.isNumeric()) return t;
         JL5TypeSystem ts = (JL5TypeSystem) t.typeSystem();
-        
+
         if (ts.isPrimitiveWrapper(t)) {
             return ts.primitiveTypeOfWrapper(t);
         }
