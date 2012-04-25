61c745a94457c02b2938ba3f2093c70a9fcd64e5
Stephen Chong <schong@cs.cornell.edu>
2012-04-25 21:05:53 +0000

    A visitor for making deep copies.

---
+++ src/polyglot/visit/DeepCopy.java
@@ -0,0 +1,19 @@
+package polyglot.visit;
+
+import polyglot.ast.Node;
+import polyglot.visit.HaltingVisitor;
+import polyglot.visit.NodeVisitor;
+
+/**
+ * Returns a deep copy of the AST.
+ */
+public class DeepCopy extends HaltingVisitor {
+    public DeepCopy() {
+        super();
+    }
+
+    @Override
+	public Node leave(Node old, Node n, NodeVisitor v) {
+        return (Node)n.copy();
+    }    
+}
