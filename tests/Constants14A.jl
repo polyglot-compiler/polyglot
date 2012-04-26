ac7f62cd400692475dc0d42e43ce1dbe092debd1
Stephen Chong <schong@cs.cornell.edu>
2012-04-26 22:42:20 +0000

    Test case for constants

---
+++ tests/Constants14A.jl
@@ -0,0 +1,13 @@
+public class Constants14A { 
+    private static final int STORED = Constants14B.STORED;
+    private static final int DEFLATED = Constants14B.DEFLATED;
+    
+    public void func() {
+        switch(0) {
+            case STORED:
+                return;
+            case DEFLATED:
+                return;
+        }       
+    }
+}
---
+++ tests/Constants14B.jl
@@ -0,0 +1,4 @@
+public class Constants14B {
+    public static final int STORED = 0;
+    public static final int DEFLATED = 1;
+}
