aeb4af05b9763ebe4b1aeecc44b877dc480f5343
Stephen Chong <schong@cs.cornell.edu>
2012-04-26 20:11:49 +0000

    Clean up boxing.

--- src/polyglot/ext/jl5/visit/AutoBoxer.java
+++ src/polyglot/ext/jl5/visit/AutoBoxer.java
@@ -9,11 +9,10 @@ import polyglot.ast.TypeNode;
 import polyglot.ext.jl5.types.JL5TypeSystem;
 import polyglot.frontend.Job;
 import polyglot.types.ClassType;
+import polyglot.types.PrimitiveType;
 import polyglot.types.SemanticException;
 import polyglot.types.Type;
-import polyglot.types.TypeSystem;
 import polyglot.util.CollectionUtil;
-import polyglot.util.InternalCompilerError;
 import polyglot.visit.AscriptionVisitor;
 
 /**
@@ -32,26 +31,44 @@ public class AutoBoxer extends AscriptionVisitor {
         if(toType.isPrimitive() && !toType.isVoid() && !fromType.isPrimitive() && !fromType.isSubtype(ts.toRawType(ts.Enum()))) {
             // going from a wrapper object to a primitive type
             // translate e to e.XXXvalue() where XXX is int, long, double, etc.
-            ClassType wrapperType = ((JL5TypeSystem)ts).wrapperClassOfPrimitive(toType.toPrimitive());
-            String methodName = toType.toPrimitive().name() + "Value";
-            Call call = nf.Call(e.position(), e, methodName);
-            call = (Call)call.type(toType);
-            call = call.methodInstance(ts.findMethod(wrapperType, methodName, Collections.emptyList(), this.context().currentClass()));		    
-            return call;
+            return fromWrapToPrim(fromType, toType.toPrimitive(), e);
         }
         else if (!toType.isPrimitive() && fromType.isPrimitive() && !fromType.isVoid() && !ts.String().equals(toType)) {
             // going from a primitive value to a wrapper type.
             // translate e to XXX.valueOf(e), where XXX is the java.lang.Integer, java.lang.Double, etc.
-            String methodName = "valueOf";
-            ClassType wrapperType = ((JL5TypeSystem)ts).wrapperClassOfPrimitive(fromType.toPrimitive());
-            TypeNode tn = nf.CanonicalTypeNode(e.position(), wrapperType);
-            Call call = nf.Call(e.position(), tn, methodName, e);
-            call = (Call)call.type(wrapperType);
-            call = call.methodInstance(ts.findMethod(wrapperType, methodName, CollectionUtil.list(fromType), this.context().currentClass()));          
-            return call;
+            return fromPrimToWrap(fromType.toPrimitive(), toType, e);
 
         }
+        
         return super.ascribe(e, toType);
+    }
+
+    private Expr fromPrimToWrap(PrimitiveType fromType, Type toType, Expr e) throws SemanticException {
+        JL5TypeSystem ts = (JL5TypeSystem)this.ts;
+        String methodName = "valueOf";
+        ClassType wrapperType = ((JL5TypeSystem)ts).wrapperClassOfPrimitive(fromType);
+        TypeNode tn = nf.CanonicalTypeNode(e.position(), wrapperType);
+        Call call = nf.Call(e.position(), tn, methodName, e);
+        call = (Call)call.type(wrapperType);
+        call = call.methodInstance(ts.findMethod(wrapperType, methodName, CollectionUtil.list(fromType), this.context().currentClass()));          
+        return call;
+    }
+
+    private Expr fromWrapToPrim(Type fromType, PrimitiveType toType, Expr e) throws SemanticException {        
+        JL5TypeSystem ts = (JL5TypeSystem)this.ts;
+        ClassType wrapperType;
+        if (ts.primitiveTypeOfWrapper(fromType) != null) {
+            wrapperType = fromType.toClass();
+        }
+        else {
+            wrapperType = ((JL5TypeSystem)ts).wrapperClassOfPrimitive(toType.toPrimitive());
+        }
+        
+        String methodName = toType.toPrimitive().name() + "Value";
+        Call call = nf.Call(e.position(), e, methodName);
+        call = (Call)call.type(toType);
+        call = call.methodInstance(ts.findMethod(wrapperType, methodName, Collections.emptyList(), this.context().currentClass()));
+        return call;
     }
 
 }
