3ed88a35576eae91c556d96616d7508f7be27400
Stephen Chong <schong@cs.cornell.edu>
2012-04-26 20:10:10 +0000

    Fix ascription to handle autoboxing.

--- src/polyglot/ext/jl5/ast/JL5LocalDecl_c.java
+++ src/polyglot/ext/jl5/ast/JL5LocalDecl_c.java
@@ -8,7 +8,10 @@ import polyglot.ast.LocalDecl;
 import polyglot.ast.LocalDecl_c;
 import polyglot.ast.TypeNode;
 import polyglot.types.Flags;
+import polyglot.types.Type;
+import polyglot.types.TypeSystem;
 import polyglot.util.Position;
+import polyglot.visit.AscriptionVisitor;
 
 public class JL5LocalDecl_c extends LocalDecl_c implements LocalDecl {
 
@@ -18,5 +21,24 @@ public class JL5LocalDecl_c extends LocalDecl_c implements LocalDecl {
         super(pos, flags, type, name, init);
         this.annotations = annotations;
     }
+    
+    public Type childExpectedType(Expr child, AscriptionVisitor av) {
+        if (child == init) {
+            TypeSystem ts = av.typeSystem();
+
+            // If the RHS is an integral constant, we can relax the expected
+            // type to the type of the constant, provided that no autoboxing
+            // is involved.
+            if (ts.numericConversionValid(type.type(), child.constantValue())) {
+                if (child.type().isPrimitive() && type.type().isPrimitive()) {
+                    return child.type();
+                }
+            }
+            return type.type();
+        }
+
+        return child.type();
+    }
+
 
 }
