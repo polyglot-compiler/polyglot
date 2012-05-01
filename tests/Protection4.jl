89034e4881460d42cf5883ce589e7dcb1fda8907
Stephen Chong <schong@cs.cornell.edu>
2012-05-01 22:50:49 +0000

    Protection test case

---
+++ tests/Protection4.jl
@@ -0,0 +1,12 @@
+class N {
+    private static class O extends M {
+        public void func() {
+            eq();
+        }
+    }
+    static void eq() {}
+}
+
+class M {
+    private static void eq() {}
+}
