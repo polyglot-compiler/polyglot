e523eba25f22f1190891989952506964c30cd963
Jed Liu <liujed@cs.cornell.edu>
2012-08-09 18:48:29 +0000

    Fixed warnings.

--- src/polyglot/frontend/JLVersion.java
+++ src/polyglot/frontend/JLVersion.java
@@ -10,9 +10,18 @@ package polyglot.frontend;
  * Version information for the base compiler.
  */
 public class JLVersion extends polyglot.main.Version {
+    @Override
     public String name() { return "jl"; }
+
+    @Override
     public int major() { return 2; }
+
+    @Override
     public int minor() { return 5; }
+
+    @Override
     public int patch_level() { return 0; }
+
+    @Override
     public String toString() { return "2.5.0 (2012-08-08 10:39:17)"; }
 }
