5fcd48e0616eeb56d08cc344736f5c38b3abec8f
Owen Arden <owen@cs.cornell.edu>
2012-10-06 14:13:37 +0000

    Version bump: 2.5.1

--- src/polyglot/frontend/JLVersion.java
+++ src/polyglot/frontend/JLVersion.java
@@ -1,28 +1,9 @@
-/*******************************************************************************
+/*
  * This file is part of the Polyglot extensible compiler framework.
  *
  * Copyright (c) 2000-2012 Polyglot project group, Cornell University
- * Copyright (c) 2006-2012 IBM Corporation
- * All rights reserved.
- *
- * This program and the accompanying materials are made available under
- * the terms of the Eclipse Public License v1.0 which accompanies this
- * distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * This program and the accompanying materials are made available under
- * the terms of the Lesser GNU Public License v2.0 which accompanies this
- * distribution.
- * 
- * The development of the Polyglot project has been supported by a
- * number of funding sources, including DARPA Contract F30602-99-1-0533,
- * monitored by USAF Rome Laboratory, ONR Grants N00014-01-1-0968 and
- * N00014-09-1-0652, NSF Grants CNS-0208642, CNS-0430161, CCF-0133302,
- * and CCF-1054172, AFRL Contract FA8650-10-C-7022, an Alfred P. Sloan 
- * Research Fellowship, and an Intel Research Ph.D. Fellowship.
- *
- * See README for contributors.
- ******************************************************************************/
+ * Copyright (c) 2006-2007 IBM Corporation
+ */
 package polyglot.frontend;
 
 /**
@@ -30,27 +11,17 @@ package polyglot.frontend;
  */
 public class JLVersion extends polyglot.main.Version {
     @Override
-    public String name() {
-        return "jl";
-    }
+    public String name() { return "jl"; }
 
     @Override
-    public int major() {
-        return 2;
-    }
+    public int major() { return 2; }
 
     @Override
-    public int minor() {
-        return 5;
-    }
+    public int minor() { return 5; }
 
     @Override
-    public int patch_level() {
-        return 0;
-    }
+    public int patch_level() { return 1; }
 
     @Override
-    public String toString() {
-        return "2.5.0 (2012-08-08 10:39:17)";
-    }
+    public String toString() { return "2.5.1 (2012-10-04 18:38:53)"; }
 }
