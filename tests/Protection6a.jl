9791bb6a0c36b073c440d46e1db284f22f587beb
Stephen Chong <schong@cs.cornell.edu>
2013-06-03 18:44:48 +0000

    Test case for protection with nested classes

---
+++ tests/Protection6a.jl
@@ -0,0 +1,9 @@
+package one;
+
+public class Protection6a {
+    protected static class TXRX {
+        public final boolean field = true;
+    }
+
+    public static abstract class Receiver extends TXRX {}
+}
---
+++ tests/Protection6b.jl
@@ -0,0 +1,9 @@
+package two;
+
+import one.Protection6a;
+
+public class Protection6b extends Protection6a.Receiver {
+    public boolean getField() {
+        return field;
+    }
+}
