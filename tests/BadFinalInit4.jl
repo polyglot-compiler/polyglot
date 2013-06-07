1450d8d9bb1b4d7f2c0bf8c1e2935eafa38f69e2
Stephen Chong <schong@cs.cornell.edu>
2013-06-07 15:25:52 +0000

    Fixing test case

--- tests/BadFinalInit4.jl
+++ tests/BadFinalInit4.jl
@@ -1,7 +1,9 @@
 class BadFinalInit4 {
     void foo(final int i) {
 	if (false) {
-	    i = 1; // bad: i might have already been assigned to, as it is a formal
+	    i = 1; // bad: i might have already been assigned to, as it is a formal. However,
+	           // according to the language spec, i is both definitely assigned and
+	           // definitely unassigned, since this is dead code. So this should be allowed.
 	}
     }
 }
