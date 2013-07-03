8636031bd2902f963dcb773947ddbcfb0089becd
Owen Arden <owen@cs.cornell.edu>
2013-07-03 14:06:42 +0000

    Don't clear enum flags when translating JL5 to JL5.

--- src/polyglot/ext/jl5/translate/EnumDeclToExt_c.java
+++ src/polyglot/ext/jl5/translate/EnumDeclToExt_c.java
@@ -28,7 +28,6 @@ package polyglot.ext.jl5.translate;
 import polyglot.ast.Node;
 import polyglot.ext.jl5.ast.JL5EnumDecl;
 import polyglot.ext.jl5.ast.JL5NodeFactory;
-import polyglot.ext.jl5.types.JL5Flags;
 import polyglot.translate.ExtensionRewriter;
 import polyglot.translate.ext.ClassDeclToExt_c;
 import polyglot.translate.ext.ToExt;
@@ -42,7 +41,7 @@ public class EnumDeclToExt_c extends ClassDeclToExt_c implements ToExt {
     public Node toExt(ExtensionRewriter rw) throws SemanticException {
         JL5EnumDecl cd = (JL5EnumDecl) node();
         return ((JL5NodeFactory) rw.to_nf()).EnumDecl(cd.position(),
-                                                      JL5Flags.clearAnnotation(JL5Flags.clearEnum(cd.flags())),
+                                                      cd.flags(),
                                                       cd.annotationElems(),
                                                       cd.id(),
                                                       cd.superClass(),
