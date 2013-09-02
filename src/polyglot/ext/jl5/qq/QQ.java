c168368770e745ab4f8ece3b9b326a705ab3d059
Owen Arden <owen@cs.cornell.edu>
2013-09-02 21:41:06 +0000

    Experimental support for JL5 Quasiquoting.
    This commit provides initial support for a JL5 quasiquoter that can handle JL5
    syntax.  Barely tested, so your mileage may vary. Drop me a note if something
    doesn't work the way you want it to. -- owen

---
+++ src/polyglot/ext/jl5/qq/QQ.java
@@ -0,0 +1,33 @@
+package polyglot.ext.jl5.qq;
+
+import polyglot.ast.Node;
+import polyglot.ast.NodeFactory;
+import polyglot.frontend.ExtensionInfo;
+import polyglot.lex.Lexer;
+import polyglot.qq.QQParser;
+import polyglot.types.TypeSystem;
+import polyglot.util.ErrorQueue;
+import polyglot.util.Position;
+
+public class QQ extends polyglot.qq.QQ {
+
+    public QQ(ExtensionInfo ext) {
+        super(ext);
+    }
+
+    public QQ(ExtensionInfo ext, Position pos) {
+        super(ext, pos);
+    }
+
+    @Override
+    protected Node parse(int kind, String fmt, Object... subst) {
+        System.out.println("HERE");
+        return super.parse(kind, fmt, subst);
+    }
+
+    @Override
+    protected QQParser parser(Lexer lexer, TypeSystem ts, NodeFactory nf,
+            ErrorQueue eq) {
+        return new polyglot.ext.jl5.qq.Grm(lexer, ts, nf, eq);
+    }
+}
