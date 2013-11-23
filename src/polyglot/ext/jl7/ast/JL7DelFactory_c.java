d2f1fa1a2c6add55bb53c3890ba4f10607c4a10f
Stephen Chong <schong@cs.cornell.edu>
2013-11-23 23:05:38 +0000

    Handling switches on strings.

--- src/polyglot/ext/jl7/ast/JL7DelFactory_c.java
+++ src/polyglot/ext/jl7/ast/JL7DelFactory_c.java
@@ -1,7 +1,13 @@
 package polyglot.ext.jl7.ast;
 
+import polyglot.ast.JL;
 import polyglot.ext.jl5.ast.JL5DelFactory_c;
 
 public class JL7DelFactory_c extends JL5DelFactory_c implements JL7DelFactory {
+
+    @Override
+    protected JL delCaseImpl() {
+        return new JL7CaseDel();
+    }
 
 }
--- src/polyglot/ext/jl7/ast/JL7ExtFactory_c.java
+++ src/polyglot/ext/jl7/ast/JL7ExtFactory_c.java
@@ -1,5 +1,6 @@
 package polyglot.ext.jl7.ast;
 
+import polyglot.ast.Ext;
 import polyglot.ext.jl5.ast.JL5ExtFactory_c;
 
 public class JL7ExtFactory_c extends JL5ExtFactory_c implements JL7ExtFactory {
@@ -14,6 +15,16 @@ public class JL7ExtFactory_c extends JL5ExtFactory_c implements JL7ExtFactory {
     @Override
     public JL7ExtFactory nextExtFactory() {
         return (JL7ExtFactory) super.nextExtFactory();
+    }
+
+    @Override
+    protected Ext extCaseImpl() {
+        return new JL7CaseExt();
+    }
+
+    @Override
+    protected Ext extSwitchImpl() {
+        return new JL7SwitchExt();
     }
 
 }
