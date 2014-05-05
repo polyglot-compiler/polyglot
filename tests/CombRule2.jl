dc8d4522a0edd46e91ebe03aaaa1b13f987eab8f
Nate Nystrom <nate.nystrom@usi.ch>
2014-05-05 22:49:55 +0200

    Added 3 tests for the so-called "comb rule". Methods inherited from superclasses should hide methods in outer classes. Javac 1.7 does not correctly implement this behavior. Seems to be fixed in Javac 1.8 and it works in Polyglot.

---
+++ tests/CombRule2.jl
@@ -0,0 +1,27 @@
+// Test based on JLS Example 6.5.7.1-1.
+// A method inherited from a superclass should hide methods 
+// with the same name in an enclosing class.
+// The rule is described in Section 15.12.1 of the JLS
+// http://docs.oracle.com/javase/specs/jls/se7/html/jls-15.html#jls-15.12.1
+//
+// javac 1.7 does not correctly implement this rule.
+class CombRule2Super {
+    void f2(String s)       {}
+    void f3(String s)       {}
+    void f3(int i1, int i2) {}
+}
+
+class CombRule1 {
+    void f1(int i) {}
+    void f2(int i) {}
+    void f3(int i) {}
+
+    void m() {
+        new CombRule2Super() {
+            {
+                f2(0);  // compile-time error
+            }
+        };
+    }
+}
+
