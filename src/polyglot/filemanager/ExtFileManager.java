polyglot-compiler/polyglot

commit 803710e4ca572fdf05b6cbb80c6df787f1b31b72

Fixed resource leak by closing CodeWriter when finishing translation.
Use flag ordering properly.

src/polyglot/frontend/TargetFactory.java
@@ -98,33 +98,32 @@ public class TargetFactory {
         }
 
         try {
+            JavaFileObject outputFile;
             if (outputExtension.equals("java")) {
-                if (packageName != null && !packageName.equals("")) {
-                    return fileManager.getJavaFileForOutput(outputLocation,
-                                                            packageName + "."
-                                                                    + className,
-                                                            Kind.SOURCE,
-                                                            null);
-                }
-                return fileManager.getJavaFileForOutput(outputLocation,
-                                                        className,
-                                                        Kind.SOURCE,
-                                                        null);
+                outputFile =
+                        fileManager.getJavaFileForOutput(outputLocation,
+                                                         !"".equals(packageName)
+                                                                 ? packageName
+                                                                         + "."
+                                                                         + className
+                                                                 : className,
+                                                         Kind.SOURCE,
+                                                         null);
             }
             else {
-                FileObject outputFile =
-                        fileManager.getFileForOutput(outputLocation,
-                                                     packageName,
-                                                     className + "."
-                                                             + outputExtension,
-                                                     null);
-
-                if (source != null
-                        && fileManager.isSameFile(source, outputFile)) {
-                    throw new InternalCompilerError("The output file is the same as the source file");
-                }
-                return (JavaFileObject) outputFile;
+                outputFile =
+                        (JavaFileObject) fileManager.getFileForOutput(outputLocation,
+                                                                      packageName,
+                                                                      className
+                                                                              + "."
+                                                                              + outputExtension,
+                                                                      null);
             }
+
+            if (source != null && fileManager.isSameFile(source, outputFile)) {
+                throw new InternalCompilerError("The output file is the same as the source file");
+            }
+            return outputFile;
         }
         catch (IOException e) {
             throw new InternalCompilerError("Error creating output file for "

skel/tests/pthScript
@@ -27,5 +27,5 @@
 #                       compiler; is always enclosed in quotes.
 
 skelpkg.ExtensionInfo "-d out" {
-    Hello.sx ;
+    Hello.sx;
 }

src/polyglot/translate/ext/ArrayAccessAssignToExt_c.java
@@ -25,7 +25,6 @@
  ******************************************************************************/
 package polyglot.translate.ext;
 
-import polyglot.ast.ArrayAccess;
 import polyglot.ast.ArrayAccessAssign;
 import polyglot.ast.Node;
 import polyglot.translate.ExtensionRewriter;
@@ -39,7 +38,7 @@ public class ArrayAccessAssignToExt_c extends ToExt_c {
     public Node toExt(ExtensionRewriter rw) throws SemanticException {
         ArrayAccessAssign n = (ArrayAccessAssign) node();
         return rw.to_nf().ArrayAccessAssign(n.position(),
-                                            (ArrayAccess) n.left(),
+                                            n.left(),
                                             n.operator(),
                                             n.right());
     }

src/polyglot/types/Flags.java
@@ -27,14 +27,13 @@
 package polyglot.types;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+import polyglot.util.InternalCompilerError;
 import polyglot.util.SerialVersionUID;
 
 /**
@@ -48,46 +47,31 @@ public class Flags implements Serializable {
     protected Set<String> flags;
 
     protected static class FlagComparator implements Comparator<String> {
-        protected static List<String> order =
-                new ArrayList<>(Arrays.asList("public",
-                                              "private",
-                                              "protected",
-                                              "static",
-                                              "final",
-                                              "synchronized",
-                                              "transient",
-                                              "native",
-                                              "interface",
-                                              "abstract",
-                                              "volatile",
-                                              "strictfp"));
+        protected static final FlagComparator instance = new FlagComparator();
+        protected static Map<String, Integer> ordering = new HashMap<>();
+        protected static Map<Integer, String> revOrdering = new HashMap<>();
 
         @Override
         public int compare(String o1, String o2) {
-            if (o1.equals(o2)) return 0;
-
-            for (int i = 0; i < order.size(); i++) {
-                if (o1.equals(order.get(i))) return -1;
-                if (o2.equals(order.get(i))) return 1;
-            }
-
+            if (ordering.containsKey(o1) && ordering.containsKey(o2))
+                return ordering.get(o1) - ordering.get(o2);
             return o1.compareTo(o2);
         }
     }
 
     public static final Flags NONE = new Flags();
     public static final Flags PUBLIC = createFlag("public", null);
-    public static final Flags PRIVATE = createFlag("private", null);
     public static final Flags PROTECTED = createFlag("protected", null);
+    public static final Flags PRIVATE = createFlag("private", null);
+    public static final Flags ABSTRACT = createFlag("abstract", null);
     public static final Flags STATIC = createFlag("static", null);
     public static final Flags FINAL = createFlag("final", null);
     public static final Flags SYNCHRONIZED = createFlag("synchronized", null);
-    public static final Flags TRANSIENT = createFlag("transient", null);
     public static final Flags NATIVE = createFlag("native", null);
-    public static final Flags INTERFACE = createFlag("interface", null);
-    public static final Flags ABSTRACT = createFlag("abstract", null);
-    public static final Flags VOLATILE = createFlag("volatile", null);
     public static final Flags STRICTFP = createFlag("strictfp", null);
+    public static final Flags TRANSIENT = createFlag("transient", null);
+    public static final Flags VOLATILE = createFlag("volatile", null);
+    public static final Flags INTERFACE = createFlag("interface", null);
 
     /** All access flags. */
     protected static final Flags ACCESS_FLAGS = PUBLIC.set(PRIVATE)
@@ -109,38 +93,37 @@ public class Flags implements Serializable {
     }
 
     public static void addToOrder(String name, Flags after) {
-        List<String> order = FlagComparator.order;
-        boolean added = false;
+        Map<String, Integer> ordering = FlagComparator.ordering;
+        Map<Integer, String> revOrdering = FlagComparator.revOrdering;
+        if (ordering.containsKey(name))
+            throw new InternalCompilerError("Flag " + name + "already added.");
 
-        if (after == null) {
-            order.add(name);
-        }
-        else if (after.flags.isEmpty()) {
-            order.add(0, name);
-        }
+        int index;
+        if (after == null)
+            index = ordering.size() + 1;
         else {
-            for (ListIterator<String> i = order.listIterator(); i.hasNext();) {
-                String s = i.next();
-                after = after.clear(new Flags(s));
-                if (after.flags.isEmpty()) {
-                    i.add(name);
-                    added = true;
-                    break;
-                }
+            index = 0;
+            for (String s : after.flags) {
+                int si = ordering.get(s);
+                if (si > index) index = si;
             }
-
-            if (!added) {
-                // shouldn't happen
-                order.add(name);
+            // shift indices of existing flags
+            for (int i = ordering.size(); i > index; i--) {
+                String s = revOrdering.get(i);
+                ordering.put(s, i + 1);
+                revOrdering.put(i + 1, s);
             }
+            index++;
         }
+        ordering.put(name, index);
+        revOrdering.put(index, name);
     }
 
     /**
-     * Effects: returns a new accessflags object with no accessflags set.
+     * Effects: returns a new access flags object with no access flags set.
      */
     protected Flags() {
-        this.flags = new TreeSet<>();
+        this.flags = new TreeSet<>(FlagComparator.instance);
     }
 
     protected Flags(String name) {

src/polyglot/util/CodeWriter.java
@@ -39,7 +39,7 @@ import java.io.IOException;
  * Line breaks can have different levels, which is useful for implementing
  * things like "miser mode" layout.
  */
-public abstract class CodeWriter {
+public abstract class CodeWriter implements AutoCloseable {
     /**
      * Print the string {@code s} verbatim on the output stream.
      * @param s the string to print.
@@ -165,6 +165,7 @@ public abstract class CodeWriter {
      * Flush all formatted text, reset formatter state, and
      * close the underlying writer.
      */
+    @Override
     public abstract void close() throws IOException;
 
     /** Like {@code flush}, but passing {@code format=false}

src/polyglot/visit/Translator.java
@@ -251,7 +251,6 @@ public class Translator extends PrettyPrinter implements Copy<Translator> {
                 if (decl.flags().isPublic() && decl != first) {
                     // We hit a new exported declaration, open a new file.
                     // But, first close the old file.
-                    w.flush();
                     w.close();
 
                     of = tf.outputFileObject(pkg, decl.name(), sfn.source());
@@ -268,7 +267,7 @@ public class Translator extends PrettyPrinter implements Copy<Translator> {
                 }
             }
 
-            w.flush();
+            w.close();
             return true;
         }
         catch (IOException e) {

commit 28e2d1031c89dd7f5c9499db2496ba3498b68933

Compiler-generated sources are now marked as being as such: Sources are now marked as being USER_SPECIFIED, DEPENDENCY, or COMPILER_GENERATED. This is a representation change. As a result, a some methods and constructors are now deprecated.
Tweaked things to avoid using aforementioned deprecated code.

This fixes a build error in Jif caused by commit
803710e4ca572fdf05b6cbb80c6df787f1b31b72.

src/polyglot/filemanager/ExtFileManager.java
@@ -53,6 +53,7 @@ import javax.tools.StandardJavaFileManager;
 
 import polyglot.frontend.ExtensionInfo;
 import polyglot.frontend.FileSource;
+import polyglot.frontend.Source;
 import polyglot.main.Main;
 import polyglot.main.Report;
 import polyglot.types.reflect.ClassFile;
@@ -436,31 +437,47 @@ public class ExtFileManager extends
 
     @Override
     public FileSource fileSource(String fileName) throws IOException {
-        return fileSource(extInfo.getOptions().source_path, fileName, false);
+        return fileSource(extInfo.getOptions().source_path,
+                          fileName,
+                          Source.Kind.DEPENDENCY);
     }
 
+    @Deprecated
     @Override
     public FileSource fileSource(String fileName, boolean userSpecified)
             throws IOException {
-        return fileSource(extInfo.getOptions().source_path,
-                          fileName,
-                          userSpecified);
+        return fileSource(fileName, userSpecified
+                ? Source.Kind.USER_SPECIFIED : Source.Kind.DEPENDENCY);
+    }
+
+    @Override
+    public FileSource fileSource(String fileName,
+            polyglot.frontend.Source.Kind kind) throws IOException {
+        return fileSource(extInfo.getOptions().source_path, fileName, kind);
     }
 
     @Override
     public FileSource fileSource(Location location, String fileName)
             throws IOException {
-        return fileSource(location, fileName, false);
+        return fileSource(location, fileName, Source.Kind.DEPENDENCY);
+    }
+
+    @Deprecated
+    @Override
+    public FileSource fileSource(Location location, String fileName,
+            boolean userSpecified) throws IOException {
+        return fileSource(location, fileName, userSpecified
+                ? Source.Kind.USER_SPECIFIED : Source.Kind.DEPENDENCY);
     }
 
     @Override
     public FileSource fileSource(Location location, String fileName,
-            boolean userSpecified) throws IOException {
+            polyglot.frontend.Source.Kind kind) throws IOException {
         File f = new File(fileName);
         FileSource sourceFile;
         FileObject fo = null;
         String key;
-        if (userSpecified) {
+        if (kind == Source.Kind.USER_SPECIFIED) {
             key = fileName;
             f = f.getAbsoluteFile();
             sourceFile = loadedSources.get(key);
@@ -483,7 +500,7 @@ public class ExtFileManager extends
                 throw new FileNotFoundException("File: " + fileName
                         + " not found.");
         }
-        sourceFile = extInfo.createFileSource(fo, userSpecified);
+        sourceFile = extInfo.createFileSource(fo, kind);
         String[] exts = extInfo.fileExtensions();
         boolean ok = false;
 
@@ -525,12 +542,11 @@ public class ExtFileManager extends
             Report.report(2, "Loading class from " + sourceFile);
 
         if (sourceFile != null) {
-            if (!sourceFile.userSpecified() && userSpecified) {
-                sourceFile.setUserSpecified(true);
-            }
+            sourceFile.setKind(kind);
             loadedSources.put(key, sourceFile);
             return sourceFile;
         }
+
         return sourceFile;
     }
 
@@ -580,7 +596,7 @@ public class ExtFileManager extends
             if (fo == null) continue;
 
             try {
-                source = extInfo.createFileSource(fo, false);
+                source = extInfo.createFileSource(fo, Source.Kind.DEPENDENCY);
                 if (Report.should_report(Report.loader, 2))
                     Report.report(2, "Loading " + className + " from " + source);
 

src/polyglot/frontend/AbstractExtensionInfo.java
@@ -41,6 +41,7 @@ import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
 import polyglot.filemanager.ExtFileManager;
 import polyglot.filemanager.FileManager;
+import polyglot.frontend.Source.Kind;
 import polyglot.frontend.goals.Goal;
 import polyglot.main.Options;
 import polyglot.main.Version;
@@ -221,10 +222,17 @@ public abstract class AbstractExtensionInfo implements ExtensionInfo {
         return new ClassFile_c(f, code, this);
     }
 
+    @Deprecated
     @Override
     public FileSource createFileSource(FileObject f, boolean user)
             throws IOException {
-        return new Source_c(f, user);
+        return createFileSource(f, user ? Kind.USER_SPECIFIED : Kind.DEPENDENCY);
+    }
+
+    @Override
+    public FileSource createFileSource(FileObject fo, Kind kind)
+            throws IOException {
+        return new Source_c(fo, kind);
     }
 
     @Override

src/polyglot/frontend/Compiler.java
@@ -37,6 +37,7 @@ import java.util.List;
 
 import javax.tools.JavaFileObject;
 
+import polyglot.frontend.Source.Kind;
 import polyglot.main.Options;
 import polyglot.types.reflect.ClassFileLoader;
 import polyglot.util.CodeWriter;
@@ -126,7 +127,8 @@ public class Compiler {
                     // mark this source as being explicitly specified
                     // by the user.
                     FileSource source =
-                            source_loader.fileSource(sourceName, true);
+                            source_loader.fileSource(sourceName,
+                                                     Kind.USER_SPECIFIED);
 
                     sources.add(source);
                 }

src/polyglot/frontend/ExtensionInfo.java
@@ -36,6 +36,7 @@ import javax.tools.JavaFileManager.Location;
 import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
 import polyglot.filemanager.FileManager;
+import polyglot.frontend.Source.Kind;
 import polyglot.frontend.goals.Goal;
 import polyglot.main.Options;
 import polyglot.translate.ext.ToExt;
@@ -140,9 +141,16 @@ public interface ExtensionInfo {
     /** Create class file for a file object. */
     ClassFile createClassFile(FileObject f, byte[] code) throws IOException;
 
-    /** Create file source for a file object. */
+    /**
+     * Create file source for a file object.
+     * @deprecated Use {@link #createFileSource(FileObject, Kind)} instead.
+     */
+    @Deprecated
     FileSource createFileSource(FileObject fo, boolean userSpecified)
             throws IOException;
+
+    /** Create file source for a file object. */
+    FileSource createFileSource(FileObject fo, Kind kind) throws IOException;
 
     /** Produce a class factory for this language extension. */
     ClassFileLoader classFileLoader();

src/polyglot/frontend/Source.java
@@ -31,9 +31,30 @@ import javax.tools.FileObject;
  * Interface for defining source files
  */
 public interface Source extends FileObject {
+    static enum Kind {
+        /** Indicates the source file was specified by the user. */
+        USER_SPECIFIED,
+
+        /** Indicates the source file was pulled in as a dependency. */
+        DEPENDENCY,
+
+        /** Indicates the source file was generated by the compiler. */
+        COMPILER_GENERATED
+    }
+
+    /**
+     * @deprecated Use {@link #setKind(Kind)} instead.
+     */
+    @Deprecated
     void setUserSpecified(boolean userSpecified);
 
     boolean userSpecified();
+
+    boolean compilerGenerated();
+
+    void setKind(Kind kind);
+
+    Kind kind();
 
     String name();
 

src/polyglot/frontend/SourceLoader.java
@@ -51,6 +51,8 @@ public interface SourceLoader {
     boolean packageExists(Location location, String name);
 
     /**
+     * Obtains a FileSource for a dependency.
+     * 
      * @param fileName
      *            A source file name
      * @return A FileSource if a source file {@code fileName} exists in
@@ -67,11 +69,26 @@ public interface SourceLoader {
      * @return A FileSource if a source file {@code fileName} exists in
      *         source path
      * @throws IOException
+     * @deprecated Use {@link #fileSource(String, Source.Kind)} instead.
      */
+    @Deprecated
     FileSource fileSource(String fileName, boolean userSpecified)
             throws IOException;
 
     /**
+     * @param fileName
+     *            A source file name
+     * @param kind
+     *            How the source was found
+     * @return A FileSource if a source file {@code fileName} exists in
+     *         source path
+     * @throws IOException
+     */
+    FileSource fileSource(String fileName, Source.Kind kind) throws IOException;
+
+    /**
+     * Obtains a FileSource for a dependency.
+     * 
      * @param location
      *            A location to search a source file in
      * @param fileName
@@ -91,9 +108,24 @@ public interface SourceLoader {
      *            If the user has specified this source file explicitly
      * @return A FileSource if a source file {@code fileName} exists
      * @throws IOException
+     * @deprecated Use {@link #fileSource(Location, String, Source.Kind)} instead. 
      */
+    @Deprecated
     FileSource fileSource(Location location, String fileName,
             boolean userSpecified) throws IOException;
+
+    /**
+     * @param location
+     *            A location to search a source file in
+     * @param kind
+     *            How the source was found
+     * @param userSpecified
+     *            If the user has specified this source file explicitly
+     * @return A FileSource if a source file {@code fileName} exists
+     * @throws IOException
+     */
+    FileSource fileSource(Location location, String fileName, Source.Kind kind)
+            throws IOException;
 
     /**
      * Load the source file for the given (possibly nested) class name using the

src/polyglot/frontend/Source_c.java
@@ -37,21 +37,51 @@ import polyglot.util.InternalCompilerError;
 
 public class Source_c extends ForwardingFileObject<FileObject> implements
         FileSource {
-    protected boolean user_specified;
 
+    protected Kind kind;
+
+    /**
+     * @Deprecated Use {@link #Source_c(FileObject, Kind)} instead.
+     */
+    @Deprecated
     protected Source_c(FileObject f, boolean userSpecified) {
-        super(f);
-        this.user_specified = userSpecified;
+        this(f, userSpecified ? Kind.USER_SPECIFIED : Kind.DEPENDENCY);
     }
 
+    protected Source_c(FileObject f, Kind kind) {
+        super(f);
+        this.kind = kind;
+    }
+
+    @Deprecated
     @Override
     public void setUserSpecified(boolean userSpecified) {
-        this.user_specified = userSpecified;
+        if (userSpecified) {
+            setKind(Kind.USER_SPECIFIED);
+        }
+        else if (userSpecified()) {
+            setKind(Kind.DEPENDENCY);
+        }
     }
 
     @Override
     public boolean userSpecified() {
-        return user_specified;
+        return kind == Kind.USER_SPECIFIED;
+    }
+
+    @Override
+    public boolean compilerGenerated() {
+        return kind == Kind.COMPILER_GENERATED;
+    }
+
+    @Override
+    public void setKind(Kind kind) {
+        this.kind = kind;
+    }
+
+    @Override
+    public Kind kind() {
+        return kind;
     }
 
     @Override

src/polyglot/frontend/TargetFactory.java
@@ -120,7 +120,8 @@ public class TargetFactory {
                                                                       null);
             }
 
-            if (source != null && fileManager.isSameFile(source, outputFile)) {
+            if (source != null && !source.compilerGenerated()
+                    && fileManager.isSameFile(source, outputFile)) {
                 throw new InternalCompilerError("The output file is the same as the source file");
             }
             return outputFile;