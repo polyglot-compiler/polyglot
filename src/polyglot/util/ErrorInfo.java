b8d0d793b2d03e32d8661c641599c4c158503165
Jed Liu <liujed@cs.cornell.edu>
2014-07-29 14:47:50 -0400

    Documented ErrorInfo.getMessage() and ErrorInfo.getErrorString().

--- src/polyglot/util/ErrorInfo.java
+++ src/polyglot/util/ErrorInfo.java
@@ -13,12 +13,12 @@
  * This program and the accompanying materials are made available under
  * the terms of the Lesser GNU Public License v2.0 which accompanies this
  * distribution.
- * 
+ *
  * The development of the Polyglot project has been supported by a
  * number of funding sources, including DARPA Contract F30602-99-1-0533,
  * monitored by USAF Rome Laboratory, ONR Grants N00014-01-1-0968 and
  * N00014-09-1-0652, NSF Grants CNS-0208642, CNS-0430161, CCF-0133302,
- * and CCF-1054172, AFRL Contract FA8650-10-C-7022, an Alfred P. Sloan 
+ * and CCF-1054172, AFRL Contract FA8650-10-C-7022, an Alfred P. Sloan
  * Research Fellowship, and an Intel Research Ph.D. Fellowship.
  *
  * See README for contributors.
@@ -38,8 +38,8 @@ public class ErrorInfo {
     public static final int DEBUG = 7;
 
     protected static String[] errorStrings = { "Warning", "Internal Error",
-            "I/O Error", "Lexical Error", "Syntax Error", "Semantic Error",
-            "Post-compiler Error", "Debug" };
+        "I/O Error", "Lexical Error", "Syntax Error", "Semantic Error",
+        "Post-compiler Error", "Debug" };
 
     protected int kind;
     protected String message;
@@ -55,6 +55,9 @@ public class ErrorInfo {
         return kind;
     }
 
+    /**
+     * @return the error message.
+     */
     public String getMessage() {
         return message;
     }
@@ -63,6 +66,10 @@ public class ErrorInfo {
         return position;
     }
 
+    /**
+     * @return a string representation of the error kind (e.g., "Warning",
+     * "Lexical Error", "Syntax Error", etc.).
+     */
     public String getErrorString() {
         return getErrorString(kind);
     }
