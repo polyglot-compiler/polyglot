polyglot-compiler/polyglot

commit e4403c131f5615bfd27a224c777bab7fcf77a2e8

Fixed bugs in handling of arguments.
  - Argument defaults are now processed before user-specified
    arguments. This avoids the defaults from inadventently clobbering
    user-specified arguments.

  - Switches now explicitly specify their default state. Formerly, this
    was assumed to be the negation of the state that results when the
    switch appears on the command line, but this isn't always the case.
    (e.g., Jif has both -robust and -nonrobust switches which
    conceptually map to the same boolean variable that is 'true' by
    default.)

src/polyglot/main/Options.java
@@ -559,13 +559,18 @@ public class Options {
      *          The set of source filenames provided on the command line.
      */
     final protected void applyArgs(Set<String> source) throws UsageError {
-        Set<OptFlag<?>> seen = new LinkedHashSet<>();
+        // Set up defaults first.
+        for (OptFlag<?> flag : flags) {
+            Arg<?> arg = flag.defaultArg(arguments);
+            if (arg != null) handleArg(arg);
+        }
+
+        // Let user-specified arguments override the defaults.
         for (Arg<?> arg : arguments) {
             if (arg.flag == null) {
                 handleSourceArg(arg, source);
             }
             else {
-                seen.add(arg.flag);
                 try {
                     handleArg(arg);
                 }
@@ -576,14 +581,6 @@ public class Options {
                     throw new InternalCompilerError("Error while handling arg "
                             + arg + " created by " + arg.flag().getClass(), e);
                 }
-            }
-        }
-        for (OptFlag<?> flag : flags) {
-            if (seen.contains(flag))
-                continue;
-            else {
-                Arg<?> arg = flag.defaultArg(arguments);
-                if (arg != null) handleArg(arg);
             }
         }
     }

src/polyglot/main/OptFlag.java
@@ -645,54 +645,148 @@ public abstract class OptFlag<T> implements Comparable<OptFlag<T>> {
      */
     public static class Switch extends OptFlag<Boolean> {
         /**
-         * Does this switch turn something on or off?
-         * For example a switch for flag "-doSomething" would
-         * have on == true. A switch for flag "--dontDoSomething"
-         * would have on == false. In both cases, the Boolean argument
-         * produced by this Swtich would represent whether we do something.
+         * Conceptually, Switches represent boolean variables. This is the value
+         * of that variable when the user specifies the switch on the command
+         * line.
          */
-        protected final boolean on;
+        protected final boolean value;
+
+        /**
+         * Conceptually, Switches represent boolean variables. This is the
+         * (default) value of that variable when the user does not specify the
+         * switch on the command line. Note this can be <i>the same</i> as
+         * {@link #value} if a switch is enabled by default.
+         */
+        protected final boolean defaultValue;
 
         @Override
         public Arg<Boolean> handle(String[] args, int index) {
-            return createArg(index, on);
+            return createArg(index, value);
         }
 
         @Override
         public Arg<Boolean> defaultArg() {
-            return createDefault(!on);
+            return createDefault(defaultValue);
         }
 
-        public Switch(Kind kind, String id, String usage, boolean on) {
+        /**
+         * @param value
+         *          the value of the switch variable when the users specifies
+         *          the switch on the command line.
+         */
+        public Switch(Kind kind, String id, String usage, boolean value) {
+            this(kind, id, usage, value, !value);
+        }
+
+        /**
+         * @param value
+         *          the value of the switch variable when the users specifies
+         *          the switch on the command line.
+         *
+         * @param defaultValue
+         *          the value of the switch variable when the users does not
+         *          specify the switch on the command line. This can be <i>the
+         *          same</i> as {@code value} if the switch is enabled by
+         *          default.
+         */
+        public Switch(Kind kind, String id, String usage, boolean value,
+                boolean defaultValue) {
             super(kind, id, null, usage);
-            this.on = on;
+            this.value = value;
+            this.defaultValue = defaultValue;
         }
 
         public Switch(Kind kind, String id, String usage) {
             this(kind, id, usage, true);
         }
 
-        public Switch(Kind kind, String[] ids, String usage, boolean on) {
+        /**
+         * @param value
+         *          the value of the switch variable when the users specifies
+         *          the switch on the command line.
+         */
+        public Switch(Kind kind, String[] ids, String usage, boolean value) {
+            this(kind, ids, usage, value, !value);
+        }
+
+        /**
+         * @param value
+         *          the value of the switch variable when the users specifies
+         *          the switch on the command line.
+         *
+         * @param defaultValue
+         *          the value of the switch variable when the users does not
+         *          specify the switch on the command line. This can be <i>the
+         *          same</i> as {@code value} if the switch is enabled by
+         *          default.
+         */
+        public Switch(Kind kind, String[] ids, String usage, boolean value,
+                boolean defaultValue) {
             super(kind, ids, null, usage);
-            this.on = on;
+            this.value = value;
+            this.defaultValue = defaultValue;
         }
 
         public Switch(Kind kind, String[] ids, String usage) {
             this(kind, ids, usage, true);
         }
 
-        public Switch(String id, String usage, boolean on) {
+        /**
+         * @param value
+         *          the value of the switch variable when the users specifies
+         *          the switch on the command line.
+         */
+        public Switch(String id, String usage, boolean value) {
+            this(id, usage, value, !value);
+        }
+
+        /**
+         * @param value
+         *          the value of the switch variable when the users specifies
+         *          the switch on the command line.
+         *
+         * @param defaultValue
+         *          the value of the switch variable when the users does not
+         *          specify the switch on the command line. This can be <i>the
+         *          same</i> as {@code value} if the switch is enabled by
+         *          default.
+         */
+        public Switch(String id, String usage, boolean value,
+                boolean defaultValue) {
             super(id, null, usage);
-            this.on = on;
+            this.value = value;
+            this.defaultValue = defaultValue;
         }
 
         public Switch(String id, String usage) {
             this(id, usage, true);
         }
 
-        public Switch(String[] ids, String usage, boolean on) {
+        /**
+         * @param value
+         *          the value of the switch variable when the users specifies
+         *          the switch on the command line.
+         */
+        public Switch(String[] ids, String usage, boolean value) {
+            this(ids, usage, value, !value);
+        }
+
+        /**
+         * @param value
+         *          the value of the switch variable when the users specifies
+         *          the switch on the command line.
+         *
+         * @param defaultValue
+         *          the value of the switch variable when the users does not
+         *          specify the switch on the command line. This can be <i>the
+         *          same</i> as {@code value} if the switch is enabled by
+         *          default.
+         */
+        public Switch(String[] ids, String usage, boolean value,
+                boolean defaultValue) {
             super(ids, null, usage);
-            this.on = on;
+            this.value = value;
+            this.defaultValue = defaultValue;
         }
 
         public Switch(String[] ids, String usage) {

commit 255cfc09720e3d906586abfb50a42571e66ab110

fixing a bug introduced in e4403c131f5615bfd27a224c777bab7fcf77a2e8

src/polyglot/main/Options.java
@@ -763,7 +763,7 @@ public class Options {
     }
 
     protected void addBootCP(List<File> value) {
-        bootclasspathDirectories().addAll(value);
+        bootclasspathDirectories().addAll(0, value);
     }
 
     protected void setSourcepath(List<File> value) {