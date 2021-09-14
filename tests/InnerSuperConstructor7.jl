9d97db60753dff9a89be77f758aade40e55debfd
Sam <sam@developersam.com>
2021-09-14 12:34:35 -0400

    Change outdated test assertions
    The changed tests assert something that don't necessarily need to be true, since the resulting code can be compiled/not compiled as expected

--- tests/InnerSuperConstructor7.jl
+++ tests/InnerSuperConstructor7.jl
@@ -5,13 +5,7 @@ class A {
 class AA extends A {
     class BB extends B {
 	BB() {
-	    super(); // BAD: since BB is a subtype of A, the
-                     // supertype's enclosing instance of A,
-	             // is the instance of BB, which is
-	             // equivalent to "this.super()", which 
-	             // is an illegal use of "this".
-	             // It should be explicitly written 
-	             // "(AA.this).super()"
+	    super();
 	}
     }
 }
