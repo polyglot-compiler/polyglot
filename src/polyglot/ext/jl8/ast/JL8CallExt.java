1989c63b3b6faf2e147b57932066df42e4177c05
Sam <sam@developersam.com>
2021-09-28 10:38:12 -0400

    Create empty JL8 extensions
    These are the nodes where lambdas are allowed to appear.

---
+++ src/polyglot/ext/jl8/ast/JL8CallExt.java
@@ -0,0 +1,7 @@
+package polyglot.ext.jl8.ast;
+
+import polyglot.util.SerialVersionUID;
+
+public class JL8CallExt extends JL8ProcedureCallExt {
+    private static final long serialVersionUID = SerialVersionUID.generate();
+}
---
+++ src/polyglot/ext/jl8/ast/JL8CastExt.java
@@ -0,0 +1,7 @@
+package polyglot.ext.jl8.ast;
+
+import polyglot.util.SerialVersionUID;
+
+public class JL8CastExt extends JL8Ext {
+    private static final long serialVersionUID = SerialVersionUID.generate();
+}
