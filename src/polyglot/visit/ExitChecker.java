0ec6f3f40a50b5de00c638b7eb37e71d8b235b62
Sam <sam@developersam.com>
2021-10-18 15:10:03 -0400

    Reformat

--- src/polyglot/visit/ExitChecker.java
+++ src/polyglot/visit/ExitChecker.java
@@ -31,7 +31,6 @@ import java.util.Map;
 import java.util.Set;
 
 import polyglot.ast.CodeNode;
-import polyglot.ast.MethodDecl;
 import polyglot.ast.NodeFactory;
 import polyglot.ast.Return;
 import polyglot.ast.Returnable;
